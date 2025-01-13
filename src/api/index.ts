6320042c525e12cefd13b814047a5235cdde9a30
shrugs <mattgcondon@gmail.com>
2025-01-13 13:04:45 -0600

    wip: custom graphql schema loaded

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,15 @@
 import { ponder } from "ponder:registry";
-import { graphql } from "ponder";
+import { default as schema } from "ponder:schema";
 
-ponder.use("/graphql", graphql());
+import { createMiddleware } from "hono/factory";
+import { buildGraphQLSchema } from "./graphql";
+import { graphql } from "./middleware";
+
+// inject our custom schema into the hono context, to be used in middleware.ts
+const overrideGraphqlSchemaMiddleware = createMiddleware(async (c, next) => {
+  c.set("graphqlSchema", buildGraphQLSchema(schema));
+  return await next();
+});
+
+ponder.use(overrideGraphqlSchemaMiddleware);
 ponder.use("/", graphql());
