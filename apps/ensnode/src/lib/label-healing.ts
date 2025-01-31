a3d67309afd9e3c8cea2c94562c117aa2d4f8c62
Tomasz Kopacki <tomasz@kopacki.net>
2025-01-31 13:18:17 +0100

    refactor: define a defualt ENSRainbow URL

--- apps/ensnode/src/lib/label-healing.ts
+++ apps/ensnode/src/lib/label-healing.ts
@@ -1,4 +1,5 @@
 import { isUnknownLabel, unknownLabelAsHex } from "ensnode-utils/subname-helpers";
+import { Hex } from "viem";
 
 interface ENSRainbow {
   /**
@@ -16,57 +17,50 @@ interface ENSRainbow {
  * @param ensRainbowApiUrl
  * @returns
  */
-function createLabelHealing(ensRainbowApiUrl: string | undefined): ENSRainbow {
+function createLabelHealing(ensRainbowApiUrl: URL): ENSRainbow {
   /**
    * Fetches the healed label for the given unknown label.
-   * @param label any label
+   * @param labelhash a hash of the unknown label
    * @returns healed label if available
    */
-  async function fetchHealedLabel(label: string) {
-    try {
-      const ensRainbowApiUrlParsed = ensRainbowApiUrl ? new URL(ensRainbowApiUrl) : null;
+  async function fetchHealedLabel(labelhash: Hex) {
+    const response = await fetch(new URL(`/v1/heal/${labelhash}`, ensRainbowApiUrl).toString());
 
-      // If the ENSRainbow API URL is not provided, we can't heal the label
-      if (!ensRainbowApiUrlParsed) {
-        console.warn("LabelHealing: no ENSRainbow API URL was provided.");
-        return null;
-      }
+    if (!response.ok) {
+      throw new Error(`Failed to fetch healed label: ${response.statusText}`);
+    }
 
+    return response.text();
+  }
+
+  return {
+    async heal(label, name) {
       // first, let's make sure this is an encoded unknown label
       if (!isUnknownLabel(label)) {
         return null;
       }
 
-      const response = await fetch(
-        new URL(`/v1/heal/${unknownLabelAsHex(label)}`, ensRainbowApiUrlParsed).toString(),
-      );
+      try {
+        const healedLabel = await fetchHealedLabel(unknownLabelAsHex(label));
 
-      if (!response.ok) {
-        console.error("Failed to heal label", label, response.statusText);
+        if (!healedLabel) {
+          return null;
+        }
+
+        // If the name is provided, try to heal it as well
+        const healedName = name ? name.replaceAll(label, healedLabel) : null;
+
+        return [healedLabel, healedName] as [typeof label, typeof name];
+      } catch (error: any) {
+        console.error("Failed to heal label", label, error.message);
         return null;
       }
-
-      return response.text();
-    } catch (error) {
-      console.error("Failed to heal label", label, error);
-      return null;
-    }
-  }
-
-  return {
-    async heal(label, name) {
-      const healedLabel = await fetchHealedLabel(label);
-
-      if (!healedLabel) {
-        return null;
-      }
-
-      // If the name is provided, try to heal it as well
-      const healedName = name ? name.replaceAll(label, healedLabel) : null;
-
-      return [healedLabel, healedName] as [typeof label, typeof name];
     },
   } satisfies ENSRainbow;
 }
 
-export const labelHealing = createLabelHealing(process.env.ENSRAINBOW_API_URL);
+const DEFAULT_ENSRAINBOW_API_URL = "https://api.ensrainbow.io";
+
+export const labelHealing = createLabelHealing(
+  new URL(process.env.ENSRAINBOW_API_URL || DEFAULT_ENSRAINBOW_API_URL),
+);
