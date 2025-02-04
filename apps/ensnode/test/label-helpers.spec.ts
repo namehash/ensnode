1d82a06f69afd9e0b504a17ee57eae0c412ef7a0
Tomasz Kopacki <tomasz@kopacki.net>
2025-02-04 18:17:43 +0100

    fix(code-style): apply auto-formatting

--- apps/ensnode/test/label-helpers.spec.ts
+++ apps/ensnode/test/label-helpers.spec.ts
@@ -1,11 +1,10 @@
-import { describe, it, expect } from "vitest";
+import { describe, expect, it } from "vitest";
 
 import { heal } from "../src/lib/label-helpers";
 
 describe("heal", () => {
   it("heals a labelhash", async () => {
-    const labelhash =
-      "0xaf2caa1c2ca1d027f1ac823b529d0a67cd144264b2789fa2ea4d63a67c7103cc";
+    const labelhash = "0xaf2caa1c2ca1d027f1ac823b529d0a67cd144264b2789fa2ea4d63a67c7103cc";
     const label = await heal(labelhash);
 
     expect(label).toEqual("vitalik");
