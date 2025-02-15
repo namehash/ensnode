2ab201ed5ad6fbc194f663182ef6cb83441f2852
lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>
2025-02-15 14:49:52 +0400

    Fix typecheck

--- packages/ensnode-utils/src/cache.spec.ts
+++ packages/ensnode-utils/src/cache.spec.ts
@@ -4,18 +4,18 @@ import { LruCache } from "./cache";
 describe("LruCache", () => {
   it("throws Error if capacity is not an integer", () => {
     expect(() => {
-      new LruCache<string>(1.5);
+      new LruCache<string, string>(1.5);
     }).toThrow();
   });
 
   it("throws Error if capacity < 0", () => {
     expect(() => {
-      new LruCache<string>(-1);
+      new LruCache<string, string>(-1);
     }).toThrow();
   });
 
   it("enforces capacity 0", () => {
-    const lru = new LruCache<string>(0);
+    const lru = new LruCache<string, string>(0);
 
     lru.set("key1", "value");
 
@@ -25,7 +25,7 @@ describe("LruCache", () => {
   });
 
   it("enforces capacity 1", () => {
-    const lru = new LruCache<string>(1);
+    const lru = new LruCache<string, string>(1);
 
     lru.set("key1", "value");
     lru.set("key2", "value");
@@ -37,7 +37,7 @@ describe("LruCache", () => {
   });
 
   it("enforces capacity > 1", () => {
-    const lru = new LruCache<string>(2);
+    const lru = new LruCache<string, string>(2);
 
     lru.set("key1", "value");
     lru.set("key2", "value");
@@ -51,7 +51,7 @@ describe("LruCache", () => {
   });
 
   it("remembers up to capacity most recently read keys", () => {
-    const lru = new LruCache<string>(2);
+    const lru = new LruCache<string, string>(2);
 
     lru.set("key1", "value");
     lru.set("key2", "value");
@@ -66,7 +66,7 @@ describe("LruCache", () => {
   });
 
   it("clears cached values", () => {
-    const lru = new LruCache<string>(1);
+    const lru = new LruCache<string, string>(1);
     lru.set("key1", "value");
     lru.set("key2", "value");
     lru.clear();
