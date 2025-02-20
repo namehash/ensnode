6a248f10de925cedf131d4c9d4214d6dcb2cc4e0
Tomasz Kopacki <tomasz@kopacki.net>
2025-02-20 21:07:36 +0100

    feat(openapi): use swagger ui for tracking api endpoints

--- apps/ensnode/src/api/index.ts
+++ apps/ensnode/src/api/index.ts
@@ -3,8 +3,14 @@ import schema from "ponder:schema";
 import { graphql as subgraphGraphQL } from "@ensnode/ponder-subgraph/middleware";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { describeRoute, openAPISpecs } from "hono-openapi";
+import { resolver, validator as zValidator } from "hono-openapi/zod";
 import { client, graphql as ponderGraphQL } from "ponder";
+import { swaggerUI } from "@hono/swagger-ui";
 import { ensNodeMetadata } from "../lib/middleware";
+import { z } from "zod";
+// For extending the Zod schema with OpenAPI properties
+import "zod-openapi/extend";
 
 const app = new Hono();
 
@@ -12,26 +18,100 @@ const app = new Hono();
 app.use(
   cors({
     origin: "*",
-  }),
+  })
 );
+
+const packageJson = await import("../../package.json").then((m) => m.default);
+const OPEN_API_MANIFEST_PATH = "/api/openapi.json";
+app.get(
+  OPEN_API_MANIFEST_PATH,
+  openAPISpecs(app, {
+    documentation: {
+      info: {
+        title: packageJson.name,
+        version: packageJson.version,
+        description: packageJson.description,
+      },
+      servers: [
+        {
+          url: process.env.PUBLIC_SERVICE_URL,
+        },
+      ],
+    },
+  })
+);
+
+// Use the middleware to serve Swagger UI at /ui
+app.get("/swagger", swaggerUI({ url: OPEN_API_MANIFEST_PATH }));
 
 // use ENSNode middleware at /metadata
 app.get(
   "/metadata",
+  describeRoute({
+    description: "Metadata describing the ENSNode service instance",
+    responses: {
+      200: {
+        description: "Metadata object",
+        content: {
+          "text/json": {
+            schema: resolver(
+              z.any().openapi({
+                type: "object",
+              })
+            ),
+          },
+        },
+      },
+    },
+  }),
   ensNodeMetadata({
     publicClients,
-  }),
+  })
 );
 
 // use ponder client support
 app.use("/sql/*", client({ db, schema }));
 
 // use ponder middleware at root
-app.use("/ponder", ponderGraphQL({ db, schema }));
+app.get(
+  "/ponder",
+  describeRoute({
+    description: "GraphiQL UI for the ENSNode service instance",
+  }),
+  ponderGraphQL({ db, schema })
+);
+app.post(
+  "/ponder",
+  describeRoute({
+    description: "GraphQL API for the ENSNode service instance",
+  }),
+  ponderGraphQL({ db, schema }),
+
+  zValidator(
+    "json",
+    z.object({
+      query: z.string().default(`
+      {
+  domains(orderBy: "createdAt", orderDirection: "desc") {
+    items {
+      name
+      labelName
+      createdAt
+      expiryDate
+    }
+  }
+  
+}`),
+      variables: z.object({}).optional(),
+    })
+  )
+);
 
 // use root to redirect to the ENSAdmin website with the current server URL as ensnode parameter
 app.use("/", async (ctx) =>
-  ctx.redirect(`https://admin.ensnode.io/about?ensnode=${process.env.SERVER_URL}`),
+  ctx.redirect(
+    `https://admin.ensnode.io/about?ensnode=${process.env.PUBLIC_SERVICE_URL}`
+  )
 );
 
 // use our custom graphql middleware at /subgraph
@@ -55,7 +135,11 @@ app.use(
           schema.fusesSet,
           schema.expiryExtended,
         ],
-        RegistrationEvent: [schema.nameRegistered, schema.nameRenewed, schema.nameTransferred],
+        RegistrationEvent: [
+          schema.nameRegistered,
+          schema.nameRenewed,
+          schema.nameTransferred,
+        ],
         ResolverEvent: [
           schema.addrChanged,
           schema.multicoinAddrChanged,
@@ -75,7 +159,7 @@ app.use(
         "Resolver.events": "ResolverEvent",
       },
     },
-  }),
+  })
 );
 
 export default app;
