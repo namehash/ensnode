c4bbb2bbbe62924f56c43c0a44a2895d217de263
Jamie Barton <jamie@notrab.dev>
2025-02-22 05:50:06 +0000

    feat: apply rapide theme

--- docs/ensnode.io/astro.config.mjs
+++ docs/ensnode.io/astro.config.mjs
@@ -1,10 +1,13 @@
 import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
+import starlightThemeRapide from 'starlight-theme-rapide'
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     starlight({
+      plugins: [starlightThemeRapide()],
       title: "ENSNode",
       social: {
         github: "https://github.com/namehash/ensnode",
@@ -52,6 +55,7 @@ export default defineConfig({
       editLink: {
         baseUrl: "https://github.com/namehash/ensnode/edit/main/docs/ensnode.io",
       },
+
     }),
   ],
 });
