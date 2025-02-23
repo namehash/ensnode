257f3b69991e4d250d4d1b63f7aa8ddbb2585633
Jamie Barton <jamie@notrab.dev>
2025-02-23 12:19:35 +0000

    sitemap and other config tidy

--- docs/ensnode.io/astro.config.mjs
+++ docs/ensnode.io/astro.config.mjs
@@ -1,61 +1,9 @@
-import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
-import starlightThemeRapide from 'starlight-theme-rapide'
 
+import { sitemap } from "./config/integrations/sitemap";
+import { starlight } from "./config/integrations/starlight";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      plugins: [starlightThemeRapide()],
-      title: "ENSNode",
-      social: {
-        github: "https://github.com/namehash/ensnode",
-      },
-      sidebar: [
-        {
-          label: "ENSNode",
-          items: [
-            {
-              label: "Quickstart",
-              slug: "ensnode/quickstart",
-            },
-            {
-              label: "Guides",
-              autogenerate: { directory: "ensnode/guides" },
-              collapsed: true,
-            },
-            {
-              label: "Reference",
-              autogenerate: { directory: "ensnode/reference" },
-              collapsed: true,
-            },
-          ],
-        },
-        {
-          label: "ENSRainbow",
-          items: [
-            {
-              label: "Quickstart",
-              slug: "ensrainbow/quickstart",
-            },
-            {
-              label: "Guides",
-              autogenerate: { directory: "ensrainbow/guides" },
-              collapsed: true,
-            },
-            {
-              label: "Reference",
-              autogenerate: { directory: "ensrainbow/reference" },
-              collapsed: true,
-            },
-          ],
-        },
-      ],
-      editLink: {
-        baseUrl: "https://github.com/namehash/ensnode/edit/main/docs/ensnode.io",
-      },
-
-    }),
-  ],
+  site: "https://ensnode.io",
+  integrations: [starlight(), sitemap()],
 });
