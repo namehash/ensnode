daaaf1a5acd80819929d37b108267098754b17c3
y3drk <ziebura.jedrzej@gmail.com>
2025-02-24 10:44:12 +0100

    Slider animated with user interaction removed

--- docs/ensrainbow/src/components/HomepageSlider.tsx
+++ docs/ensrainbow/src/components/HomepageSlider.tsx
@@ -38,29 +38,29 @@ export default function HomepageSlider() {
     return (
         <div className="wrapper" ref={imageContainer}>
             <img className="image1" alt="unknown-name" src={unknown_name.src}/>
-            <img className="image2" alt="known-name" src={known_name.src} style={{
-                clipPath: `polygon(0 0, ${imageRevealFraction * 100}% 0, ${imageRevealFraction * 100}% 100%, 0 100%)`
-            }}/>
-            <div style={{
-                left: `${imageRevealFraction * 100}%`
-            }}
+            <img className="image2" alt="known-name" src={known_name.src}
+            //      style={{
+            //     clipPath: `polygon(0 0, ${imageRevealFraction * 100}% 0, ${imageRevealFraction * 100}% 100%, 0 100%)`
+            // }}
+            />
+            <div
+            //     style={{
+            //     left: `${imageRevealFraction * 100}%`
+            // }}
                  className="outerController"
             >
                 <div className="innerController">
-                    {/*TODO: try box shadow on line instead of backgrounds!*/}
-                    {/*<div className="lineBackground posRight"></div>*/}
-                    {/*<div className="lineBackground posLeft"></div>*/}
                     <div className="line"></div>
-                    <div onMouseDown={handleMouseOnController} className="controllerButton">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
-                             stroke="currentColor" className="size-6" style={{
-                                 rotate: "90deg"
-                        }}>
-                            <path strokeLinecap="round" strokeLinejoin="round"
-                                  d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"/>
-                        </svg>
+                    {/*<div onMouseDown={handleMouseOnController} className="controllerButton">*/}
+                    {/*    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}*/}
+                    {/*         stroke="currentColor" className="size-6" style={{*/}
+                    {/*             rotate: "90deg"*/}
+                    {/*    }}>*/}
+                    {/*        <path strokeLinecap="round" strokeLinejoin="round"*/}
+                    {/*              d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"/>*/}
+                    {/*    </svg>*/}
 
-                    </div>
+                    {/*</div>*/}
                 </div>
             </div>
         </div>
--- docs/ensrainbow/src/components/RainbowTest.tsx
+++
@@ -1,9 +0,0 @@
-import "../styles/slider-styles.css";
-export default function RainbowTest() {
-    return <div className="rainbowTest">
-        <p style={{
-            color: "white",
-            textAlign: 'center'
-        }}>How the 'line' works</p>
-    </div>
-}
