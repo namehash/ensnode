e267888017e2a55b46024fbed3b083ac106bb9ec
shrugs <mattgcondon@gmail.com>
2025-02-24 16:34:51 -0600

    fix: ensrainbow.io just an index, direct to ensnode website

--- docs/ensrainbow.io/astro.config.mjs
+++ docs/ensrainbow.io/astro.config.mjs
@@ -1,30 +1,9 @@
-import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
+import { sitemap } from "./config/integrations/sitemap";
+import { starlight } from "./config/integrations/starlight";
+
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "ENSRainbow Docs",
-      social: {
-        github: "https://github.com/namehash/ensnode/tree/main/apps/ensrainbow",
-      },
-      sidebar: [
-        {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            {
-              label: "Quick start",
-              slug: "guides/quick-start",
-            },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
-      ],
-    }),
-  ],
+  site: "https://ensrainbow.io",
+  integrations: [starlight(), sitemap()],
 });
