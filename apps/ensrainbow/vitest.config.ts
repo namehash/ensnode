fc7511507f6be0a4d2479657ce43ccb1387c6825
Tomasz Kopacki <tomasz@kopacki.net>
2025-02-28 19:41:01 +0100

    fix(ensrainbow): make `bun` runtime to run `vitest` test suite

--- apps/ensrainbow/vitest.config.ts
+++ apps/ensrainbow/vitest.config.ts
@@ -1,8 +1,41 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, ViteUserConfig } from "vitest/config";
+
+export type Runtime = "node" | "deno" | "bun";
+
+export function getRuntime(): Runtime {
+  if ("bun" in process.versions) return "bun";
+  if ("deno" in process.versions) return "deno";
+
+  return "node";
+}
+
+export function getPoolOptions(runtime: Runtime): ViteUserConfig["test"] {
+  if (runtime === "node") {
+    return {
+      pool: "threads",
+      poolOptions: {
+        threads: {
+          singleThread: true,
+          minThreads: 2,
+          maxThreads: 10,
+        },
+      },
+    };
+  }
+
+  return {
+    pool: "vitest-in-process-pool",
+    dangerouslyIgnoreUnhandledErrors: true,
+    coverage: {
+      enabled: false,
+    },
+  };
+}
+
 
 export default defineConfig({
   test: {
-    environment: "node",
+    ...getPoolOptions(getRuntime()),
     include: ["src/**/*.test.ts"],
     coverage: {
       provider: "v8",
