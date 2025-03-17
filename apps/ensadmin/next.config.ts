9abb673d03eee1d25d9601698f89d28617089cd0
Tomasz Kopacki <tomasz@kopacki.net>
2025-03-17 19:41:06 +0100

    feat(ensadmin): setup docker env

--- apps/ensadmin/next.config.ts
+++ apps/ensadmin/next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
+let output: NextConfig["output"];
+
+if (process.env.NEXT_BUILD_OUTPUT_STANDALONE === "true") {
+  output = "standalone";
+}
+
 const nextConfig: NextConfig = {
+  // Docker builds require the standalone output
+  // Vercel builds require the default (`undefined`) output
+  output,
   eslint: {
     ignoreDuringBuilds: true,
   },
--- apps/ensadmin/src/lib/env.ts
+++ apps/ensadmin/src/lib/env.ts
@@ -5,7 +5,32 @@ export function selectedEnsNodeUrl(params: URLSearchParams): string {
 const PREFERRED_ENSNODE_URL = "https://alpha.ensnode.io";
 
 export function preferredEnsNodeUrl(): string {
-  return process.env.PREFERRED_ENSNODE_URL || PREFERRED_ENSNODE_URL;
+  const envVarName = "NEXT_PUBLIC_PREFERRED_ENSNODE_URL";
+  const envVarValue = process.env.NEXT_PUBLIC_PREFERRED_ENSNODE_URL;
+
+  if (!envVarValue) {
+    console.warn(
+      `No preferred URL provided in ${envVarName}. Using fallback: ${PREFERRED_ENSNODE_URL}`,
+    );
+
+    return PREFERRED_ENSNODE_URL;
+  }
+
+  try {
+    return parseUrl(envVarValue).toString();
+  } catch (error) {
+    console.error(error);
+
+    throw new Error(`Invalid ${envVarName} value "${envVarValue}". It should be a valid URL.`);
+  }
+}
+
+function parseUrl(maybeUrl: string): URL {
+  try {
+    return new URL(maybeUrl);
+  } catch (error) {
+    throw new Error(`Invalid URL: ${maybeUrl}`);
+  }
 }
 
 export async function ensAdminVersion(): Promise<string> {
