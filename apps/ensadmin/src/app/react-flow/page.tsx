6aeda8c86cbab9cb73d3adf6b4d5e64ab197d01c
Jamie Barton <jamie@notrab.dev>
2025-03-26 11:58:02 +0000

    feat(ensadmin): add teaser config options

--- apps/ensadmin/src/app/react-flow/page.tsx
+++ apps/ensadmin/src/app/react-flow/page.tsx
@@ -5,10 +5,5 @@ import dynamic from "next/dynamic";
 const ReactFlowClient = dynamic(() => import("./client"), { ssr: false });
 
 export default function ReactFlowPoCPage() {
-  return (
-    <>
-      <h1 className="text-center font-bold leading-7 text-4xl p-2">React-Flow PoC</h1>
-      <ReactFlowClient />
-    </>
-  );
+  return <ReactFlowClient />;
 }
