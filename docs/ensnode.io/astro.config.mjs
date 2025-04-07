c782b62d13e30497eed7a11c4b980ad96e355af2
Jamie Barton <jamie@notrab.dev>
2025-04-07 14:48:04 +0100

    fix: use valid redirects

--- docs/ensnode.io/astro.config.mjs
+++ docs/ensnode.io/astro.config.mjs
@@ -21,6 +21,11 @@ export default defineConfig({
     },
   },
   redirects: {
-    "/ensnode/[...slug]": "/docs/[...slug]",
+    "/ensnode": "/docs",
+    "/ensnode/deploying/railway": "/docs/deploying/railway",
+    "/ensnode/concepts/what-is-ensnode": "/docs/concepts/what-is-ensnode",
+    "/ensnode/running/ens-test-env": "/docs/running/ens-test-env",
+    "/ensnode/concepts/what-is-the-ens-subgraph":
+      "/docs/concepts/what-is-the-ens-subgraph",
   },
 });
