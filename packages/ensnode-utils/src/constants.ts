5aabb5fd57f496cf511ae361a50c63c96605d725
shrugs <mattgcondon@gmail.com>
2025-04-08 16:42:57 -0500

    Update packages/ensnode-utils/src/constants.ts
    Co-authored-by: lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>

--- packages/ensnode-utils/src/constants.ts
+++ packages/ensnode-utils/src/constants.ts
@@ -6,7 +6,7 @@ export const ROOT_NODE: Node = namehash("");
 /**
  * A set of nodes whose children are used for reverse resolution.
  *
- * Useful for filtering new domains by whether they're a reverse node for a specific address.
+ * Useful for identifying if a domain is used for reverse resolution.
  * See apps/ensindexer/src/handlers/Registry.ts for context.
  */
 export const REVERSE_ROOT_NODES: Set<Node> = new Set([namehash("addr.reverse")]);
