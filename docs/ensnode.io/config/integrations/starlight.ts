7cb7323e193d0a466463ed5562128e13dfede4e2
y3drk <ziebura.jedrzej@gmail.com>
2025-04-10 14:30:04 +0200

    Font adjustments in buttons, docs button content change & switching to Inter font in general

--- docs/ensnode.io/config/integrations/starlight.ts
+++ docs/ensnode.io/config/integrations/starlight.ts
@@ -12,7 +12,8 @@ export function starlight(): AstroIntegration {
       Hero: "./src/components/overrides/Hero.astro",
       TableOfContents: "./src/components/overrides/TableOfContents.astro",
     },
-    customCss: ["./src/styles/globals.css", "./src/styles/pagination.css"],
+    customCss: ["./src/styles/globals.css", "./src/styles/pagination.css", "@fontsource/inter/100.css", "@fontsource/inter/200.css", "@fontsource/inter/300.css", "@fontsource/inter/400.css", "@fontsource/inter/500.css",
+      "@fontsource/inter/600.css", "@fontsource/inter/700.css", "@fontsource/inter/800.css", "@fontsource/inter/900.css"],
     plugins: [
       starlightLlmsTxt(),
       starlightSidebarTopics([
