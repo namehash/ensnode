0d8ca95f3f0ed0f0b201133143bf438dd7f14eba
Jamie Barton <jamie@notrab.dev>
2025-04-14 14:21:44 +0100

    apply fixes

--- apps/ensadmin/src/app/@breadcrumbs/inspector/page.tsx
+++ apps/ensadmin/src/app/@breadcrumbs/inspector/page.tsx
@@ -4,11 +4,11 @@ export default function BreadcrumbsInspectorPage() {
   return (
     <BreadcrumbItem>
       <BreadcrumbPage className="flex items-center gap-1">
-        Protocol Inspector{" "}
-        <div className="relative -top-1.5 bg-black w-fit h-fit p-[2.8px] rounded-[2.8px] flex-shrink-0">
-          <p className="text-white not-italic font-semibold pb-[0.5px] text-[6.857px] leading-[7.619px] sm:text-[8.409px] sm:leading-[9.343px]">
+        ENS Protocol Inspector{" "}
+        <div className="hidden lg:inline relative -top-1.5 bg-black w-fit h-fit p-[2.8px] rounded-[2.8px] flex-shrink-0">
+          <span className="text-white not-italic font-semibold pb-[0.5px] text-[6.857px] leading-[7.619px] sm:text-[8.409px] sm:leading-[9.343px] cursor-pointer">
             teaser
-          </p>
+          </span>
         </div>
       </BreadcrumbPage>
     </BreadcrumbItem>
