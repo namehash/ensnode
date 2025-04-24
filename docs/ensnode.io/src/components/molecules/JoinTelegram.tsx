4e9f0a6baab9853720645618a81c67832e6a5512
y3drk <ziebura.jedrzej@gmail.com>
2025-04-24 14:45:08 +0200

    Fixes for desktop layout between 750-900px

--- docs/ensnode.io/src/components/molecules/JoinTelegram.tsx
+++ docs/ensnode.io/src/components/molecules/JoinTelegram.tsx
@@ -6,15 +6,15 @@ import MobileTelegramBanner from "../../assets/telegram_mobile_image.svg";
 
 export default function JoinTelegram() {
   return (
-    <div className="bg-telegram_bg max-w-[1216px] w-full h-fit flex flex-col sm:flex-row justify-center items-center flex-nowrap gap-10 sm:gap-5 sm:pl-20 sm:pr-5 pt-10 sm:pt-0 rounded-[20px] overflow-hidden">
-      <div className="w-4/5 h-fit flex flex-col flex-nowrap items-center sm:items-start justify-center gap-5 sm:py-5">
+    <div className="bg-telegram_bg max-w-[1216px] w-full h-fit flex flex-col min-[900px]:flex-row justify-center items-center flex-nowrap gap-10 min-[900px]:gap-5 min-[900px]:pl-20 min-[900px]:pr-5 pt-10 min-[900px]:pt-0 rounded-[20px] overflow-hidden">
+      <div className="w-4/5 h-fit flex flex-col flex-nowrap items-center min-[900px]:items-start justify-center gap-5 min-[900px]:py-5">
         <Balancer
           as="h2"
-          className="self-stretch text-3xl sm:text-4xl leading-9 sm:leading-10 font-bold text-black text-center sm:text-left"
+          className="self-stretch text-3xl min-[900px]:text-4xl leading-9 min-[900px]:leading-10 font-bold text-black text-center min-[900px]:text-left"
         >
           Become a part of our community in Telegram
         </Balancer>
-        <p className="text-lg leading-8 sm:leading-7 font-normal text-gray-500">
+        <p className="text-lg leading-8 min-[900px]:leading-7 font-normal text-gray-500">
           Run your own ENSNode
         </p>
         <Button asChild size="medium" variant="primary">
@@ -24,12 +24,12 @@ export default function JoinTelegram() {
         </Button>
       </div>
       <img
-        className="hidden sm:flex w-auto h-[280px] shrink-0 object-contain bg-no-repeat"
+        className="hidden min-[450px]:flex w-auto h-[280px] shrink-0 object-contain bg-no-repeat"
         src={TelegramBanner.src}
         alt="Telegram Banner"
       />
       <img
-        className="sm:hidden flex w-fit h-full shrink-0 object-contain bg-no-repeat"
+        className="min-[450px]:hidden flex w-fit h-full shrink-0 object-contain bg-no-repeat"
         src={MobileTelegramBanner.src}
         alt="Telegram Banner"
       />
