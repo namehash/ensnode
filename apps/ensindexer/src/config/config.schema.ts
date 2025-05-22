429484257f4a65dc5c50ae0bccd4f1c3fdf69dc1
shrugs <mattgcondon@gmail.com>
2025-05-21 19:31:58 -0500

    fix: move validations back to validations

--- apps/ensindexer/src/config/config.schema.ts
+++ apps/ensindexer/src/config/config.schema.ts
@@ -3,6 +3,12 @@ import { prettifyError, z } from "zod/v4";
 
 import { ENSIndexerConfig, ENSIndexerEnvironment } from "@/config/types";
 import {
+  invariant_globalBlockrange,
+  invariant_requiredDatasources,
+  invariant_rpcConfigsSpecifiedForIndexedChains,
+  invariant_validContractConfigs,
+} from "@/config/validations";
+import {
   DEFAULT_ENSADMIN_URL,
   DEFAULT_ENS_DEPLOYMENT_CHAIN,
   DEFAULT_HEAL_REVERSE_ADDRESSES,
@@ -177,121 +183,10 @@ const ENSIndexerConfigSchema = z
     rpcConfigs: parseRpcConfigs(),
     databaseUrl: parseDatabaseUrl(),
   })
-  // Invariant: specified plugins' datasources are available in the specified ensDeploymentChain's ENSDeployment
-  .check((ctx) => {
-    const { value: config } = ctx;
-
-    const deployment = getENSDeployment(config.ensDeploymentChain);
-    const allPluginNames = Object.keys(PLUGIN_REQUIRED_DATASOURCES) as PluginName[];
-    const availableDatasourceNames = Object.keys(deployment) as DatasourceName[];
-    const activePluginNames = allPluginNames.filter((pluginName) =>
-      config.plugins.includes(pluginName),
-    );
-
-    // validate that each active plugin's requiredDatasources are available in availableDatasourceNames
-    for (const pluginName of activePluginNames) {
-      const requiredDatasources = PLUGIN_REQUIRED_DATASOURCES[pluginName];
-      const hasRequiredDatasources = requiredDatasources.every((datasourceName) =>
-        availableDatasourceNames.includes(datasourceName),
-      );
-
-      if (!hasRequiredDatasources) {
-        ctx.issues.push({
-          code: "custom",
-          input: config,
-          message: `Requested plugin '${pluginName}' cannot be activated for the ${
-            config.ensDeploymentChain
-          } deployment. ${pluginName} specifies dependent datasources: [${requiredDatasources.join(
-            ", ",
-          )}], but available datasources in the ${
-            config.ensDeploymentChain
-          } deployment are: [${availableDatasourceNames.join(", ")}].`,
-        });
-      }
-    }
-  })
-  // Invariant: rpcConfig is specified for each indexed chain
-  .check((ctx) => {
-    const { value: config } = ctx;
-
-    const deployment = getENSDeployment(config.ensDeploymentChain);
-
-    for (const pluginName of config.plugins) {
-      const datasourceNames = PLUGIN_REQUIRED_DATASOURCES[pluginName];
-
-      for (const datasourceName of datasourceNames) {
-        const { chain } = deployment[datasourceName];
-
-        if (!config.rpcConfigs[chain.id]) {
-          ctx.issues.push({
-            code: "custom",
-            input: config,
-            message: `Plugin '${pluginName}' indexes chain with id ${chain.id} but RPC_URL_${chain.id} is not specified.`,
-          });
-        }
-      }
-    }
-  })
-  // Invariant: if a global blockrange is defined, only one network is indexed
-  .check((ctx) => {
-    const { value: config } = ctx;
-    const { globalBlockrange } = config;
-
-    if (globalBlockrange.startBlock !== undefined || globalBlockrange.endBlock !== undefined) {
-      const deployment = getENSDeployment(config.ensDeploymentChain);
-      const indexedChainIds = uniq(
-        config.plugins
-          .flatMap((pluginName) => PLUGIN_REQUIRED_DATASOURCES[pluginName])
-          .map((datasourceName) => deployment[datasourceName])
-          .map((datasource) => datasource.chain.id),
-      );
-
-      if (indexedChainIds.length > 1) {
-        ctx.issues.push({
-          code: "custom",
-          input: config,
-          message: `ENSIndexer's behavior when indexing _multiple networks_ with a _specific blockrange_ is considered undefined (for now). If you're using this feature, you're likely interested in snapshotting at a specific END_BLOCK, and may have unintentially activated plugins that source events from multiple chains. The config currently is:
-
-  ENS_DEPLOYMENT_CHAIN=${config.ensDeploymentChain}
-  ACTIVE_PLUGINS=${config.plugins.join(",")}
-  START_BLOCK=${globalBlockrange.startBlock || "n/a"}
-  END_BLOCK=${globalBlockrange.endBlock || "n/a"}
-
-  The usage you're most likely interested in is:
-    ENS_DEPLOYMENT_CHAIN=(mainnet|sepolia|holesky) ACTIVE_PLUGINS=subgraph END_BLOCK=x pnpm run start
-  which runs just the 'subgraph' plugin with a specific end block, suitable for snapshotting ENSNode and comparing to Subgraph snapshots.
-
-  In the future, indexing multiple networks with network-specific blockrange constraints may be possible.`,
-        });
-      }
-    }
-  })
-  // Invariant: if ens-test-env is the ensDeploymentChain, ensure that its Datasources provide addresses
-  .check((ctx) => {
-    const { value: config } = ctx;
-
-    const deployment = getENSDeployment(config.ensDeploymentChain);
-    for (const datasourceName of Object.keys(deployment) as DatasourceName[]) {
-      const { contracts } = deployment[datasourceName];
-
-      // invariant: `contracts` must provide valid addresses if a filter is not provided
-      const hasAddresses = Object.values(contracts)
-        .filter((contractConfig) => "address" in contractConfig) // only ContractConfigs with `address` defined
-        .every((contractConfig) => isAddress(contractConfig.address as Address)); // must be a valid `Address`
-
-      if (!hasAddresses) {
-        throw new Error(
-          `The ENSDeployment '${
-            config.ensDeploymentChain
-          }' datasource '${datasourceName}' does not define valid addresses. This occurs if the address property of any ContractConfig in the ENSDeployment is malformed (i.e. not an Address). This is only likely to occur if you are running the 'ens-test-env' ENSDeployment outside of the context of the ens-test-env tool (https://github.com/ensdomains/ens-test-env). If you are activating the ens-test-env plugin and receive this error, NEXT_PUBLIC_DEPLOYMENT_ADDRESSES or DEPLOYMENT_ADDRESSES is not available in the env or is malformed.
-
-ENS_DEPLOYMENT_CHAIN=${config.ensDeploymentChain}
-NEXT_PUBLIC_DEPLOYMENT_ADDRESSES=${process.env.NEXT_PUBLIC_DEPLOYMENT_ADDRESSES || "undefined"}
-DEPLOYMENT_ADDRESSES=${process.env.DEPLOYMENT_ADDRESSES || "undefined"}`,
-        );
-      }
-    }
-  });
+  .check(invariant_requiredDatasources)
+  .check(invariant_rpcConfigsSpecifiedForIndexedChains)
+  .check(invariant_globalBlockrange)
+  .check(invariant_validContractConfigs);
 
 /**
  * Builds the ENSIndexer configuration object from an ENSIndexerEnvironment object
