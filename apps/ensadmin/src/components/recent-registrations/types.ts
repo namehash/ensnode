2b03de921cd8ee1d442ffe882f3825ee9a96eac0
y3drk <ziebura.jedrzej@gmail.com>
2025-05-26 14:27:10 +0200

    Define new <LatestRegistration> interface - solves 1st  point from Issue #548

--- apps/ensadmin/src/components/recent-registrations/types.ts
+++ apps/ensadmin/src/components/recent-registrations/types.ts
@@ -15,6 +15,23 @@ export interface LatestRegistrationResult {
   };
 }
 
+/**
+  A "pure" version of LatestRegistrationResult that streamlines data usage in RecentRegistrations component.
+ @params:
+ * name - string - recently registered name
+ * createdAt - number - a UNIX timestamp in seconds of when the name was originally created
+ * expiryDate - number - a UNIX timestamp in seconds of when will the current owner's tenure end
+ * owner - Address -  owner's address
+ * wrappedOwner - Address - optional - wrapped owner's address
+ **/
+export interface LatestRegistration {
+  name: string;
+  createdAt: number;
+  expiryDate: number;
+  owner:  Address;
+  wrappedOwner?: Address;
+}
+
 export interface Registration {
   registrationDate: string;
   expiryDate: string;
