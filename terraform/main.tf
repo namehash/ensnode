16a1f05ec7a590a344ec952fd175800e0124eea1
BanaSeba <sebastian.banas@appliscale.io>
2025-05-28 17:16:35 +0200

    Fixed variable distribution

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,26 +6,35 @@ locals {
   # Example indexer.holesky.terraform-test.ensnode.io
   base_domain_name = "ensnode.io"
   # US East Metal - Railway regions: https://docs.railway.com/reference/regions
-  railway_region         = "us-east4-eqdc4a"
-  heal_reverse_addresses = "false"
-  active_plugins         = "subgraph"
-  ens_deployment_chain   = "holesky"
+  railway_region = "us-east4-eqdc4a"
   ensindexer_instances = {
     holesky = {
-      subdomain_prefix = "holesky.${local.railway_environment}"
-      database_schema  = "holeskySchema-${var.ensnode_version}"
+      subdomain_prefix       = "holesky.${local.railway_environment}"
+      database_schema        = "holeskySchema-${var.ensnode_version}"
+      active_plugins         = "subgraph"
+      ens_deployment_chain   = "holesky"
+      heal_reverse_addresses = "false"
     }
     sepolia = {
-      subdomain_prefix = "sepolia.${local.railway_environment}"
-      database_schema  = "sepoliaSchema-${var.ensnode_version}"
+      subdomain_prefix       = "sepolia.${local.railway_environment}"
+      database_schema        = "sepoliaSchema-${var.ensnode_version}"
+      active_plugins         = "subgraph"
+      ens_deployment_chain   = "sepolia"
+      heal_reverse_addresses = "false"
     }
     mainnet = {
-      subdomain_prefix = "mainnet.${local.railway_environment}"
-      database_schema  = "mainnetSchema-${var.ensnode_version}"
+      subdomain_prefix       = "mainnet.${local.railway_environment}"
+      database_schema        = "mainnetSchema-${var.ensnode_version}"
+      active_plugins         = "subgraph"
+      ens_deployment_chain   = "mainnet"
+      heal_reverse_addresses = "false"
     }
     alpha = {
-      subdomain_prefix = "alpha.${local.railway_environment}"
-      database_schema  = "alphaSchema-${var.ensnode_version}"
+      subdomain_prefix       = "alpha.${local.railway_environment}"
+      database_schema        = "alphaSchema-${var.ensnode_version}"
+      active_plugins         = "subgraph,basenames,lineanames,threedns"
+      ens_deployment_chain   = "mainnet"
+      heal_reverse_addresses = "true"
     }
   }
 }
@@ -45,48 +54,23 @@ module "database" {
   railway_environment_id = railway_project.this.default_environment.id
 }
 
-module "holesky_ensindexer" {
-  source     = "./modules/ensindexer"
-  depends_on = [null_resource.health_check]
-  #Indexer specific envs
-  base_domain_name       = local.base_domain_name
-  subdomain_prefix       = "holesky.${local.railway_environment}"
-  ensnode_version        = var.ensnode_version
-  heal_reverse_addresses = local.heal_reverse_addresses
-  ensrainbow_url         = "http://$${{${railway_service.ensrainbow.name}.RAILWAY_PRIVATE_DOMAIN}}:8080"
-  database_schema        = "holeskySchema-${var.ensnode_version}"
-  active_plugins         = local.active_plugins
-  ens_deployment_chain   = local.ens_deployment_chain
-  #Common envs
-  railway_region         = local.railway_region
-  railway_token          = var.railway_token
-  railway_project_id     = railway_project.this.id
-  railway_environment_id = railway_project.this.default_environment.id
-  database_url           = "$${{${module.database.database_instance_name}.DATABASE_URL}}"
-  mainnet_rpc_url        = var.mainnet_rpc_url
-  sepolia_rpc_url        = var.sepolia_rpc_url
-  linea_rpc_url          = var.linea_rpc_url
-  holesky_rpc_url        = var.holesky_rpc_url
-  base_rpc_url           = var.base_rpc_url
-  optimism_rpc_url       = var.optimism_rpc_url
-}
-
 module "ensindexer" {
   source   = "./modules/ensindexer"
   for_each = local.ensindexer_instances
 
   depends_on = [null_resource.health_check]
   # Instance-specific configuration
-  subdomain_prefix = each.value.subdomain_prefix
-  database_schema  = each.value.database_schema
+  subdomain_prefix       = each.value.subdomain_prefix
+  database_schema        = each.value.database_schema
+  active_plugins         = each.active_plugins
+  ens_deployment_chain   = each.ens_deployment_chain
+  heal_reverse_addresses = each.heal_reverse_addresses
 
   # Common configuration (spread operator merges the map)
-  base_domain_name       = local.base_domain_name
-  ensnode_version        = var.ensnode_version
-  heal_reverse_addresses = local.heal_reverse_addresses
-  ensrainbow_url         = "http://$${{${railway_service.ensrainbow.name}.RAILWAY_PRIVATE_DOMAIN}}:8080"
-  active_plugins         = local.active_plugins
-  ens_deployment_chain   = local.ens_deployment_chain
+  base_domain_name = local.base_domain_name
+  ensnode_version  = var.ensnode_version
+  ensrainbow_url   = "http://$${{${railway_service.ensrainbow.name}.RAILWAY_PRIVATE_DOMAIN}}:8080"
+
   #Common envs
   railway_region         = local.railway_region
   railway_token          = var.railway_token
