0293c63fa18e41c581aef72e8a7a37c2771cd0e0
shrugs <mattgcondon@gmail.com>
2025-05-28 20:03:54 -0500

    fix: remove stubbed api endpoint

--- apps/ensindexer/src/api/handlers/ensnode-api.ts
+++ apps/ensindexer/src/api/handlers/ensnode-api.ts
@@ -1,38 +1,7 @@
-import { db, publicClients } from "ponder:api";
-import schema from "ponder:schema";
-import { eq } from "ponder";
-
-import { resolveForward } from "@/api/lib/forward-resolution";
-import { resolveReverse } from "@/api/lib/reverse-resolution";
-import { makeResolverId } from "@/lib/ids";
-import { DatasourceName, ENSDeployments } from "@ensnode/ens-deployments";
 import { Hono } from "hono";
-import { namehash } from "viem";
 
 const app = new Hono();
 
-const JESSE_ADDRESS = "0xFb434f97aB5Def37277505833992F2a8E065e14D";
-
-app.get("/records", async (ctx) => {
-  await resolveForward("jesse.base.eth");
-
-  // TODO: zod validation for args
-  // const data = await resolveReverse("0x849151d7D0bF1F34b70d5caD5149D28CC2308bf1");
-  const {
-    chain,
-    contracts: { ReverseResolver },
-  } = ENSDeployments.sepolia[DatasourceName.ReverseResolverBase];
-  // TODO: use const addrReverseLabel = (address: Address): Label => ;
-  const reverseNode = namehash(`${JESSE_ADDRESS.slice(2).toLowerCase()}.addr.reverse`);
-
-  const resolverId = makeResolverId(chain.id, ReverseResolver.address, reverseNode);
-
-  const resolver = await db
-    .select()
-    .from(schema.resolver)
-    .where(eq(schema.resolver.id, resolverId));
-  console.log(resolver);
-  return ctx.json({ resolver });
-});
+// TODO: some api endpoints
 
 export default app;
