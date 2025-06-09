199c29d085dea8186495965489b56727b6093a16
y3drk <ziebura.jedrzej@gmail.com>
2025-06-09 15:02:09 +0200

    ensNodeInstanceURL prop typecheck fix

--- apps/ensadmin/src/app/example-queries/components/ExampleQueriesContent.tsx
+++ apps/ensadmin/src/app/example-queries/components/ExampleQueriesContent.tsx
@@ -9,7 +9,7 @@ import Link from "next/link";
 import React from "react";
 
 interface ExampleQueriesContentProps {
-  ensNodeInstanceURL: string;
+  ensNodeInstanceURL: string | URL;
 }
 
 export function ExampleQueriesContent({ ensNodeInstanceURL }: ExampleQueriesContentProps) {
