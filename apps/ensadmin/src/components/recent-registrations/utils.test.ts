c93cc88f6f997108f458121e0be48932530df83f
y3drk <ziebura.jedrzej@gmail.com>
2025-06-23 18:10:43 +0200

    Add new testcase for <unixTimestampToDate> function's unit tests

--- apps/ensadmin/src/components/recent-registrations/utils.test.ts
+++ apps/ensadmin/src/components/recent-registrations/utils.test.ts
@@ -8,6 +8,12 @@ describe("unixTimestampToDate", () => {
     expect(() => unixTimestampToDate(invalidTimestamp)).toThrowError(/Error parsing timestamp/);
   });
 
+  it("should throw an exception for an empty string as input", () => {
+    const invalidTimestamp = "";
+
+    expect(() => unixTimestampToDate(invalidTimestamp)).toThrowError(/Error parsing timestamp/);
+  });
+
   it("should parse correct timestamp to a date object", () => {
     const validTimestamp = "1781826068";
     const expectedDate = new Date("2026-06-18T23:41:08.000Z");
