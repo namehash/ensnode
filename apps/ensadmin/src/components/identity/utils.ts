6d212f5d77d29dddac3aefb48f0a6de199dd8691
y3drk <ziebura.jedrzej@gmail.com>
2025-06-25 12:45:56 +0200

    Handpick and repopulate changes from the previous draft PR

---
+++ apps/ensadmin/src/components/identity/utils.ts
@@ -0,0 +1,44 @@
+import {ENSNamespaceId} from "@ensnode/datasources";
+
+/**
+ * Get ENS app URL for wallet address on a supported network.
+ * NOTE: not every ENS deployment has an ENS app URL.
+ * @param {ENSNamespaceId} chain - ENS Namespace identifier
+ * @returns ENS app URL for supported networks, otherwise undefined
+ */
+export function getEnsAppUrl(chain: ENSNamespaceId): URL | undefined {
+    switch (chain) {
+        case "mainnet":
+            return new URL(`https://app.ens.domains/`);
+        case "sepolia":
+            return new URL(`https://sepolia.app.ens.domains/`);
+        case "holesky":
+            return new URL(`https://holesky.app.ens.domains/`);
+        case "ens-test-env":
+            // ens-test-env cannot be served by app.ens.domains website
+            return undefined;
+    }
+}
+
+/**
+ * Get metadata URL for a given ENS deployment
+ * NOTE: not every ENS deployment has an ENS metadata URL.
+ * @param {ENSNamespaceId} chain - ENS Namespace identifier
+ * @returns metadata URL for supported networks, otherwise undefined
+ */
+export function getEnsMetadataUrl(chain: ENSNamespaceId): URL | undefined {
+    switch (chain) {
+        case "mainnet":
+            return new URL(`https://metadata.ens.domains/mainnet/avatar/`);
+        case "sepolia":
+            return new URL(`https://metadata.ens.domains/sepolia/avatar/`);
+        case "holesky":
+            // NOTE: there's no metadata api available on metadata.ens.domains website for the holesky network
+            return undefined;
+        case "ens-test-env":
+            // ens-test-env cannot be served by metadata.ens.domains website
+            return undefined;
+    }
+}
+
+//TODO: Should these functions be handling the errors and undefined results? Or maybe the <Identity /> component should handle it?
--- apps/ensadmin/src/components/recent-registrations/utils.tsx
+++ apps/ensadmin/src/components/recent-registrations/utils.tsx
@@ -1,14 +1,22 @@
 import { formatDistanceStrict, formatDistanceToNow, intlFormat } from "date-fns";
 import { millisecondsInSecond } from "date-fns/constants";
 import { useEffect, useState } from "react";
+import {Address, getAddress} from "viem";
+import {
+  Datasource,
+  ENSNamespaceId,
+  getENSNamespace
+} from "@ensnode/datasources";
+import {Registration} from "@/components/recent-registrations/types";
+import {ExternalLink} from "lucide-react";
 
 /**
  * Client-only date formatter component
  */
 export function FormattedDate({
-  date,
-  options,
-}: {
+                                date,
+                                options,
+                              }: {
   date: Date;
   options: Intl.DateTimeFormatOptions;
 }) {
@@ -38,9 +46,9 @@ export function RelativeTime({ date }: { date: Date }) {
  * Client-only duration component
  */
 export function Duration({
-  beginsAt,
-  endsAt,
-}: {
+                           beginsAt,
+                           endsAt,
+                         }: {
   beginsAt: Date;
   endsAt: Date;
 }) {
@@ -70,3 +78,82 @@ export function unixTimestampToDate(timestamp: UnixTimestampInSeconds): Date {
 
   return date;
 }
+
+/**
+ * Truncates a name's leftmost label if it's made of an address
+ */
+//TODO: improve name and description
+//TODO: refactor the code
+function truncateLeftmostLabelIfItContainsAddress(name:string) : string {
+  const leftmostLabel = name.split(".")[0];
+
+  //if name's label is an address (ex. [ec93f85a766b60d85571efc48e4b818c800218452f9ac738f796a8fc94079a57].eth) truncate it
+  if (leftmostLabel.startsWith("[") && leftmostLabel.endsWith("]")){
+    return `${leftmostLabel.slice(0, 6)}...${name.slice(-3)}${name.split(".").slice(1).join(".")}`
+  }
+
+  // otherwise return whole domain
+  return name;
+}
+
+interface RegistrationNameDisplayProps {
+  // NOTE: not every ENS namespace has an ENS app URL
+  ensAppUrl: URL | undefined;
+
+  registration: Registration;
+}
+
+/**
+ * Component to display an ENS registration.
+ * It can display a link to the ENS name, or just the name if the ENS namespace has no dedicated ENS App.
+ */
+
+//TODO: consider a different name
+//TODO: should probably be moved to /identity
+//TODO: for now this handles the case of undefined ENS app URLs - may change depending on other TODOs
+export function RegistrationNameDisplay({ registration, ensAppUrl }: RegistrationNameDisplayProps) {
+  const ensAppRegistrationPreviewUrl = ensAppUrl
+      ? new URL(registration.name, ensAppUrl)
+      : undefined;
+
+  const displayName = truncateLeftmostLabelIfItContainsAddress(registration.name);
+
+  if (ensAppRegistrationPreviewUrl) {
+    return (
+        <a
+            href={ensAppRegistrationPreviewUrl.toString()}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-1 text-blue-600 hover:underline"
+        >
+          {displayName}
+          <ExternalLink size={14} className="inline-block"/>
+        </a>
+    );
+  }
+
+  return <span>{displayName}</span>;
+}
+
+/**
+ Get an Address object of the NameWrapper contract from the root datasource of a specific namespace.
+
+ @param chainId the chain ID
+ @param namespaceId - the namespace identifier within which to find a root datasource
+ @returns the viem#Address object
+ */
+//TODO: where to use it? I don't like the prop-drilling that would be required to pass the address from <RecentRegistrations /> to getEffectiveOwner, but what's a better way to do it?
+//TODO: make sure it actually works! make sure we take the root datasource! --> Investigate how the Datasource is built once again, maybe I omitted sth earlier?
+export function getNameWrapperAddress(namespaceId: ENSNamespaceId, chainId: number): Address{
+  const datasources = Object.values(getENSNamespace(namespaceId)) as Datasource[];
+  const datasource = datasources.find((datasource) => datasource.chain.id === chainId);
+
+  if (!datasource) {
+    throw new Error(
+        `No Datasources within the "${namespaceId}" namespace are defined for Chain ID "${chainId}".`,
+    );
+  }
+
+  return getAddress(datasource.contracts.NameWrapper.address);
+}
+
