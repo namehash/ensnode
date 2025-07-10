36e9079cd74f752557a2fb6c7bb34e33d9ca5602
shrugs <mattgcondon@gmail.com>
2025-07-10 18:35:17 +0100

    disable otel in production

--- apps/ensindexer/src/api/lib/instrumentation.ts
+++ apps/ensindexer/src/api/lib/instrumentation.ts
@@ -6,24 +6,40 @@ import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 import { resourceFromAttributes } from "@opentelemetry/resources";
 import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { BatchSpanProcessor, SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import {
+  BatchSpanProcessor,
+  SimpleSpanProcessor,
+  type SpanProcessor,
+} from "@opentelemetry/sdk-trace-node";
 import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from "@opentelemetry/semantic-conventions";
+
+// Something broken with OpenTelemetry? enable debugging by uncommenting the following two lines:
+// import { DiagConsoleLogger, DiagLogLevel, diag } from "@opentelemetry/api";
+// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
+
+// always include the ProtocolTraceExporter
+// NOTE: the SimpleSpanProcessor is mandatory, we want synchronous access to ended spans
+const spanProcessors: SpanProcessor[] = [
+  new SimpleSpanProcessor(ProtocolTraceExporter.singleton()),
+];
+
+// TODO: include this in production once OTel Collector is set up
+if (process.env.NODE_ENV === "development") {
+  spanProcessors.push(
+    new BatchSpanProcessor(new OTLPTraceExporter(), {
+      scheduledDelayMillis: process.env.NODE_ENV === "development" ? 1_000 : undefined,
+    }),
+  );
+}
 
 export const sdk = new NodeSDK({
   resource: resourceFromAttributes({
     [ATTR_SERVICE_NAME]: "ensnode-api",
     [ATTR_SERVICE_VERSION]: packageJson.version,
   }),
-  spanProcessors: [
-    new SimpleSpanProcessor(ProtocolTraceExporter.singleton()),
-    new BatchSpanProcessor(new OTLPTraceExporter(), {
-      scheduledDelayMillis: process.env.NODE_ENV === "development" ? 1_000 : undefined,
-    }),
-  ],
-  metricReader: new PeriodicExportingMetricReader({ exporter: new OTLPMetricExporter() }),
+  spanProcessors,
+  // TODO: metrics, once OTel Collector is set up
+  // metricReader: new PeriodicExportingMetricReader({ exporter: new OTLPMetricExporter() }),
   // NOTE: avoiding auto-instrumentation for now as it adds complexity and can be quite noisy
   instrumentations: [],
 });
-
-// import { DiagConsoleLogger, DiagLogLevel, diag } from "@opentelemetry/api";
-// diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
