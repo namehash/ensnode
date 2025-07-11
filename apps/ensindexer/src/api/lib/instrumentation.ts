79011dd0b5aa4f96c099622ea84c4456c0e02ca7
shrugs <mattgcondon@gmail.com>
2025-07-11 18:01:07 +0100

    fix: conditional span exporting

--- apps/ensindexer/src/api/lib/instrumentation.ts
+++ apps/ensindexer/src/api/lib/instrumentation.ts
@@ -7,7 +7,11 @@ import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-proto";
 import { resourceFromAttributes } from "@opentelemetry/resources";
 // import { PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { BatchSpanProcessor, SimpleSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import {
+  BatchSpanProcessor,
+  SimpleSpanProcessor,
+  SpanProcessor,
+} from "@opentelemetry/sdk-trace-node";
 import { ATTR_SERVICE_NAME, ATTR_SERVICE_VERSION } from "@opentelemetry/semantic-conventions";
 
 // Something broken with OpenTelemetry? enable debug logging with OTEL_DEBUG=anything
@@ -15,18 +19,26 @@ if (!!process.env.OTEL_DEBUG) {
   diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL);
 }
 
+const spanProcessors: SpanProcessor[] = [
+  // NOTE: the SimpleSpanProcessor is mandatory, we want synchronous access to ended spans
+  new SimpleSpanProcessor(ProtocolTraceExporter.singleton()),
+];
+
+if (!!process.env.OTEL_EXPORTER_OTLP_ENDPOINT) {
+  // only export spans to collector iff OTEL_EXPORTER_OTLP_ENDPOINT is defined
+  spanProcessors.push(
+    new BatchSpanProcessor(new OTLPTraceExporter(), {
+      scheduledDelayMillis: process.env.NODE_ENV === "development" ? 1_000 : undefined,
+    }),
+  );
+}
+
 export const sdk = new NodeSDK({
   resource: resourceFromAttributes({
     [ATTR_SERVICE_NAME]: "ensnode-api",
     [ATTR_SERVICE_VERSION]: packageJson.version,
   }),
-  spanProcessors: [
-    // NOTE: the SimpleSpanProcessor is mandatory, we want synchronous access to ended spans
-    new SimpleSpanProcessor(ProtocolTraceExporter.singleton()),
-    new BatchSpanProcessor(new OTLPTraceExporter(), {
-      scheduledDelayMillis: process.env.NODE_ENV === "development" ? 1_000 : undefined,
-    }),
-  ],
+  spanProcessors,
   // TODO: metrics, once OTel Collector is set up
   // metricReader: new PeriodicExportingMetricReader({ exporter: new OTLPMetricExporter() }),
   // NOTE: avoiding auto-instrumentation for now as it adds complexity and can be quite noisy
