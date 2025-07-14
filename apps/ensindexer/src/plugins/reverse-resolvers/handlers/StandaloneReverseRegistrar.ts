a3ea71503511945fe964d7484a928a5b23b08ac8
shrugs <mattgcondon@gmail.com>
2025-07-14 13:41:42 +0100

    fix: restructure reverse-resolvers plugin to match new ensip spec

---
+++ apps/ensindexer/src/plugins/reverse-resolvers/handlers/StandaloneReverseRegistrar.ts
@@ -0,0 +1,24 @@
+import { ponder } from "ponder:registry";
+import { evmChainIdToCoinType } from "@ensnode/ensnode-sdk";
+
+/**
+ * Handler functions for ENSIP-19 StandaloneReverseRegistrar contracts. These contracts manage
+ * `name` records for an address, per-coinType (via chainId).
+ */
+export default function () {
+  ponder.on("StandaloneReverseRegistrar:NameForAddrChanged", async ({ context, event }) => {
+    const { addr: address, name } = event.args;
+    const coinType = evmChainIdToCoinType(context.chain.id);
+
+    console.log({
+      on: "StandaloneReverseRegistrar:NameForAddrChanged",
+      chainId: context.chain.id,
+      coinType,
+      address,
+      name,
+    });
+
+    // TODO: upsert entity representing (address, coinType) -> Name
+    // TODO: treat empty string name as deletion
+  });
+}
--- apps/ensindexer/src/plugins/reverse-resolvers/plugin.ts
+++ apps/ensindexer/src/plugins/reverse-resolvers/plugin.ts
@@ -3,11 +3,7 @@ import { PluginName } from "@ensnode/ensnode-sdk";
 import { ChainConfig, createConfig } from "ponder";
 
 import { createPlugin, getDatasourceAsFullyDefinedAtCompileTime } from "@/lib/plugin-helpers";
-import {
-  chainConfigForContract,
-  chainsConnectionConfig,
-  mergeContractConfigs,
-} from "@/lib/ponder-helpers";
+import { chainConfigForContract, chainsConnectionConfig } from "@/lib/ponder-helpers";
 
 /**
  * Describes the indexing behavior for every known ENSIP-19 L2 Reverse Resolver.
@@ -15,26 +11,59 @@ import {
 export const pluginName = PluginName.ReverseResolvers;
 
 // NOTE: const-ed to keep inferred types in createPonderConfig
-const REVERSE_RESOLVER_DATASOURCE_NAMES = [
+const ALL_REVERSE_RESOLUTION_DATASOURCE_NAMES = [
   DatasourceNames.ReverseResolverRoot,
   DatasourceNames.ReverseResolverBase,
-  // TODO: re-enable the following
-  // DatasourceNames.ReverseResolverOptimism,
-  // DatasourceNames.ReverseResolverArbitrum,
-  // DatasourceNames.ReverseResolverScroll,
-  // DatasourceNames.ReverseResolverLinea,
+  DatasourceNames.ReverseResolverLinea,
+  DatasourceNames.ReverseResolverOptimism,
+  DatasourceNames.ReverseResolverArbitrum,
+  DatasourceNames.ReverseResolverScroll,
+] as const;
+
+// all datasource names that include an L2ReverseRegistrar contract
+const L2_REVERSE_REGISTRAR_DATASOURCE_NAMES = [
+  DatasourceNames.ReverseResolverBase,
+  DatasourceNames.ReverseResolverLinea,
+  DatasourceNames.ReverseResolverArbitrum,
+  DatasourceNames.ReverseResolverOptimism,
+  DatasourceNames.ReverseResolverScroll,
 ] as const;
 
 export default createPlugin({
   name: pluginName,
-  requiredDatasourceNames: REVERSE_RESOLVER_DATASOURCE_NAMES,
+  requiredDatasourceNames: ALL_REVERSE_RESOLUTION_DATASOURCE_NAMES,
   createPonderConfig(config) {
-    const datasources = REVERSE_RESOLVER_DATASOURCE_NAMES.map((datasourceName) =>
+    const allDatasources = ALL_REVERSE_RESOLUTION_DATASOURCE_NAMES.map((datasourceName) =>
       getDatasourceAsFullyDefinedAtCompileTime(config.namespace, datasourceName),
     );
 
+    const rrRoot = getDatasourceAsFullyDefinedAtCompileTime(
+      config.namespace,
+      DatasourceNames.ReverseResolverRoot,
+    );
+    const rrBase = getDatasourceAsFullyDefinedAtCompileTime(
+      config.namespace,
+      DatasourceNames.ReverseResolverBase,
+    );
+    const rrLinea = getDatasourceAsFullyDefinedAtCompileTime(
+      config.namespace,
+      DatasourceNames.ReverseResolverLinea,
+    );
+    const rrOptimism = getDatasourceAsFullyDefinedAtCompileTime(
+      config.namespace,
+      DatasourceNames.ReverseResolverOptimism,
+    );
+    const rrArbitrum = getDatasourceAsFullyDefinedAtCompileTime(
+      config.namespace,
+      DatasourceNames.ReverseResolverArbitrum,
+    );
+    const rrScroll = getDatasourceAsFullyDefinedAtCompileTime(
+      config.namespace,
+      DatasourceNames.ReverseResolverScroll,
+    );
+
     return createConfig({
-      chains: datasources
+      chains: allDatasources
         .map((datasource) => datasource.chain)
         .reduce<Record<string, ChainConfig>>(
           (memo, chain) => ({
@@ -47,23 +76,69 @@ export default createPlugin({
       contracts: {
         // a single multi-chain ReverseResolver ContractConfig
         ReverseResolver: {
-          // each chain's ReverseResolver gets a `chain` entry via `chainConfigForContract`
-          chain: datasources.reduce((memo, datasource) => {
-            const contracts = Object.values(
-              datasource.contracts,
-            ) as (typeof datasource.contracts)[keyof typeof datasource.contracts][];
+          chain: {
+            // the Root chain's DefaultReverseResolver2
+            ...chainConfigForContract(
+              config.globalBlockrange,
+              rrRoot.chain.id,
+              rrRoot.contracts.DefaultReverseResolver2,
+            ),
+            // Base's ReverseResolver
+            ...chainConfigForContract(
+              config.globalBlockrange,
+              rrBase.chain.id,
+              rrBase.contracts.ReverseResolver,
+            ),
+          },
 
-            // treat all contract configs in this ReverseResolver* Datasource as ReverseResolvers
-            const contract = mergeContractConfigs(contracts);
+          // NOTE: all ReverseResolvers share the same abi, just use the first definition
+          // TODO: use shared Resolver abi for this
+          abi: rrRoot.contracts.DefaultReverseResolver2.abi,
+        },
 
-            return {
-              ...memo,
-              ...chainConfigForContract(config.globalBlockrange, datasource.chain.id, contract),
-            };
-          }, {}),
-
-          // NOTE: all ReverseResolvers share the same abi, so just use the first definition
-          abi: datasources[0]!.contracts.ReverseResolver.abi,
+        // a single multi-chain StandaloneReverseRegistrar ContractConfig
+        StandaloneReverseRegistrar: {
+          chain: {
+            // the Root chain's StandaloneReverseRegistrar
+            ...chainConfigForContract(
+              config.globalBlockrange,
+              rrRoot.chain.id,
+              rrRoot.contracts.DefaultReverseRegistrar,
+            ),
+            // Base's L2ReverseRegistrar
+            ...chainConfigForContract(
+              config.globalBlockrange,
+              rrBase.chain.id,
+              rrBase.contracts.L2ReverseRegistrar,
+            ),
+            // Linea's L2ReverseRegistrar
+            ...chainConfigForContract(
+              config.globalBlockrange,
+              rrLinea.chain.id,
+              rrLinea.contracts.L2ReverseRegistrar,
+            ),
+            // Optimism's L2ReverseRegistrar
+            ...chainConfigForContract(
+              config.globalBlockrange,
+              rrOptimism.chain.id,
+              rrOptimism.contracts.L2ReverseRegistrar,
+            ),
+            // Arbitrum's L2ReverseRegistrar
+            ...chainConfigForContract(
+              config.globalBlockrange,
+              rrArbitrum.chain.id,
+              rrArbitrum.contracts.L2ReverseRegistrar,
+            ),
+            // Scroll's L2ReverseRegistrar
+            ...chainConfigForContract(
+              config.globalBlockrange,
+              rrScroll.chain.id,
+              rrScroll.contracts.L2ReverseRegistrar,
+            ),
+          },
+          // NOTE: all StandaloneReverseRegistrar share the same abi, just use the first definition
+          // TODO: export this shared abi from sdk and re-use it here
+          abi: rrRoot.contracts.DefaultReverseRegistrar.abi,
         },
       },
     });
