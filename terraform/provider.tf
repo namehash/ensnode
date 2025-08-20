57003141ca4e2b6b9b1881de6c537d502e382c46
BanaSeba <sebastian.banas@appliscale.io>
2025-08-20 06:40:09 +0200

    Added Render deploy flag in case default is wrong

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -22,4 +22,5 @@ provider "render" {
   api_key                    = var.render_api_key
   owner_id                   = var.render_owner_id
   wait_for_deploy_completion = true
+  skip_deploy_after_service_update = false
 }
