5e223544d841cfcf48e7606c70c81de039ffdd04
shrugs <mattgcondon@gmail.com>
2025-08-21 17:01:36 -0500

    fix: connect redirect

--- apps/ensadmin/src/app/connect/page.tsx
+++ apps/ensadmin/src/app/connect/page.tsx
@@ -21,7 +21,7 @@ export default function ConnectPage() {
     if (!ensNodeUrl) redirect("/status");
 
     addAndSelectConnection.mutate(ensNodeUrl, {
-      onSuccess: () => redirect("/status"),
+      onSuccess: () => setTimeout(() => redirect("/status"), 0),
     });
   }, []);
 
@@ -33,7 +33,9 @@ export default function ConnectPage() {
   if (addAndSelectConnection.isError) {
     return (
       <div className="flex h-full flex-col items-center justify-center gap-4">
-        <h2 className="text-center">Something went wrong while connecting to ENSNode</h2>
+        <h2 className="text-center">
+          Something went wrong while connecting to ENSNode: {addAndSelectConnection.error.message}
+        </h2>
         <p className="flex flex-row items-center justify-center gap-2">
           <span>Provided URL:</span>
           <span className="text-center font-mono bg-gray-200 py-1 px-2">{ensNodeUrl}</span>
