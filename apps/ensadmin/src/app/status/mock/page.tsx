c48f2d02ab4cbf20936008b45e61b7cf1113151e
lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>
2025-08-22 16:09:27 +0300

    Cleanup unused import

--- apps/ensadmin/src/app/status/mock/page.tsx
+++ apps/ensadmin/src/app/status/mock/page.tsx
@@ -1,14 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   SerializedENSIndexerOverallIndexingStatus,
   deserializeENSIndexerIndexingStatus,
