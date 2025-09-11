4d88e29eef4be9ef504e8758c183b8c8840e7826
shrugs <mattgcondon@gmail.com>
2025-09-11 11:16:06 -0500

    fix: no empty string special for labelhash

--- packages/ensnode-sdk/src/shared/labelhash.test.ts
+++ packages/ensnode-sdk/src/shared/labelhash.test.ts
@@ -1,18 +1,18 @@
-import { labelhash } from "viem";
+import { keccak256, labelhash, stringToBytes } from "viem";
 import { describe, expect, it } from "vitest";
 import { LiteralLabel, encodeLabelHash } from "../ens";
 import { labelhashLiteralLabel } from "./labelhash";
 
 describe("labelhashLiteralLabel", () => {
   it("labelhashes empty string correctly", () => {
-    expect(labelhashLiteralLabel("" as LiteralLabel)).toEqual(labelhash(""));
+    expect(labelhashLiteralLabel("" as LiteralLabel)).toEqual(keccak256(stringToBytes("")));
   });
 
   it("labelhashes literal label correctly", () => {
     expect(labelhashLiteralLabel("example" as LiteralLabel)).toEqual(labelhash("example"));
   });
 
-  it("labelhashes encoded-labelhash-looking-strings", () => {
+  it("labelhashes encoded-labelhash-looking-strings as literal labels", () => {
     const encodedLabelHashLookingLabel = encodeLabelHash(labelhash("whatever")) as LiteralLabel;
     expect(labelhashLiteralLabel(encodedLabelHashLookingLabel)).not.toEqual(
       labelhash(encodedLabelHashLookingLabel),
--- packages/ensnode-sdk/src/shared/labelhash.ts
+++ packages/ensnode-sdk/src/shared/labelhash.ts
@@ -1,19 +1,13 @@
-import { keccak256, stringToBytes, zeroHash } from "viem";
+import { keccak256, stringToBytes } from "viem";
 import { LabelHash, LiteralLabel } from "../ens";
 
 /**
  * Implements the ENS `labelhash` function for Literal Labels.
  * @see https://docs.ens.domains/ensip/1
  *
- * NOTE: This function is viem/ens#labelhash but without the special-case handling of Encoded LabelHashes.
- *
  * @param label the Literal Label to hash
  * @returns the hash of the provided label
+ * @dev This function is viem/ens#labelhash but without the special-case handling of Encoded LabelHashes.
  */
-export function labelhashLiteralLabel(label: LiteralLabel): LabelHash {
-  // the labelhash of the empty string is zeroHash
-  if (label === "") return zeroHash;
-
-  // otherwise, keccak256 the label bytes
-  return keccak256(stringToBytes(label));
-}
+export const labelhashLiteralLabel = (label: LiteralLabel): LabelHash =>
+  keccak256(stringToBytes(label));
