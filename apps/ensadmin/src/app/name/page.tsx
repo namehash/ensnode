242977f8f3beb15fcfeb4c50bd9f14b31833d8ed
y3drk <ziebura.jedrzej@gmail.com>
2025-09-26 15:05:14 +0200

    Implementation of the initial version of the /name page alongside its simple UI&UX

--- apps/ensadmin/src/app/name/page.tsx
+++ apps/ensadmin/src/app/name/page.tsx
@@ -1,14 +1,67 @@
+"use client";
+
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+import {ChangeEvent, useState} from "react";
+import {useRouter} from "next/navigation";
+import {getNameDetailsRelativePath} from "@/components/identity/utils";
+import {Name} from "@ensnode/ensnode-sdk";
 
 export default function NameNavigationPage(){
+    //TODO: theoretically, the "View Name" button could just be a child of the <Link> component,
+    // but I feel like this violates how <form> should work, hence the useRouter approach
+    const router = useRouter();
+    const [searchedName, setSearchedName] = useState<string>("");
+
+    //TODO: For now pressing enter will only submit the form if the input is focused.
+    // Not sure if that satisfies this requirement:
+    // 'If the form is "submittable" then pressing enter should automatically submit the form.'
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target as HTMLFormElement);
+        const name = formData.get("ens-name") as Name;
+
+        //TODO: should we automatically check for and&or add ".eth" TLD?
+        // Seems legit if this requirement: "For now, there will be no validation or transformations
+        // on the input into this field. All inputs will be valid and interpreted literally."
+        // is to be satisfied.
+        router.push(getNameDetailsRelativePath(name));
+    };
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+
+        setSearchedName(e.target.value);
+    };
+
     return (
-        <Card>
-            <CardHeader>
-                <CardTitle>TITLE</CardTitle>
-            </CardHeader>
-            <CardContent>
-                CONTENT
-            </CardContent>
-        </Card>
+        <section className="flex flex-col gap-6 p-6">
+            <Card className="w-full">
+                <CardHeader className="sm:pb-4 max-sm:p-3">
+                    <CardTitle className="text-2xl">Explore ENS Names</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    {/*//TODO: Maybe we could create a short list of example names for users to pick from?
+                    Similarly to how it's done in the 'RecordsResolution' ?*/}
+                    <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
+                        <fieldset className="flex flex-row gap-2 justify-start items-center">
+                            <Input
+                                autoFocus
+                                type="text"
+                                required
+                                id="ens-name"
+                                name="ens-name"
+                                placeholder="Search for a name..."
+                                value={searchedName}
+                                onChange={handleChange}
+                            />
+                            <Button type="submit" disabled={searchedName.length === 0}>View Name</Button>
+                        </fieldset>
+                    </form>
+                </CardContent>
+            </Card>
+        </section>
     );
 }
