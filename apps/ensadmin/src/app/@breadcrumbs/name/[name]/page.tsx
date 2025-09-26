23ac598a0b42225d18acc8bc5cac06aa6deae0b6
y3drk <ziebura.jedrzej@gmail.com>
2025-09-26 15:04:07 +0200

    Update of the breadcrumbs for the existing 'Name Detail Page' & ensuring that 'actions' are present only for the 'Name Detail Page'

--- apps/ensadmin/src/app/@breadcrumbs/name/[name]/page.tsx
+++ apps/ensadmin/src/app/@breadcrumbs/name/[name]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { NameDisplay } from "@/components/identity/utils";
-import { BreadcrumbItem, BreadcrumbPage } from "@/components/ui/breadcrumb";
+import {BreadcrumbItem, BreadcrumbLink, BreadcrumbPage, BreadcrumbSeparator} from "@/components/ui/breadcrumb";
 import { useParams } from "next/navigation";
 
 export default function Page() {
@@ -9,10 +9,16 @@ export default function Page() {
   const name = decodeURIComponent(params.name as string);
 
   return (
-    <BreadcrumbItem>
-      <BreadcrumbPage>
-        <NameDisplay name={name} />
-      </BreadcrumbPage>
-    </BreadcrumbItem>
+      <>
+          <BreadcrumbLink href="/name" className="hidden md:block">
+              Names
+          </BreadcrumbLink>
+          <BreadcrumbSeparator className="hidden md:block" />
+          <BreadcrumbItem>
+              <BreadcrumbPage>
+                  <NameDisplay name={name} />
+              </BreadcrumbPage>
+          </BreadcrumbItem>
+      </>
   );
 }
