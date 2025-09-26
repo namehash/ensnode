bb0f5655ccfc1a0b7794c985e1b8f6961017b584
y3drk <ziebura.jedrzej@gmail.com>
2025-09-26 13:42:10 +0200

    Addition of new 'ENS Explorer' sidebar item & setup for a new /name page

---
+++ apps/ensadmin/src/app/name/page.tsx
@@ -0,0 +1,14 @@
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+
+export default function NameNavigationPage(){
+    return (
+        <Card>
+            <CardHeader>
+                <CardTitle>TITLE</CardTitle>
+            </CardHeader>
+            <CardContent>
+                CONTENT
+            </CardContent>
+        </Card>
+    );
+}
