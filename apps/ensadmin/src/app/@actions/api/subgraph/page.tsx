a1c9cab65fc8a194c27544be20c63684fad4fbb8
lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>
2025-10-01 02:18:31 +0300

    Apply PR feedback

--- apps/ensadmin/src/app/@actions/api/subgraph/page.tsx
+++ apps/ensadmin/src/app/@actions/api/subgraph/page.tsx
@@ -4,10 +4,13 @@ import { CopyButton } from "@/components/copy-button";
 import { useSelectedConnection } from "@/hooks/active/use-selected-connection";
 
 export default function ActionsSubgraphCompatPage() {
+  const { validatedSelectedConnection } = useSelectedConnection();
+
   // TODO: we need a broader refactor to recognize the difference between
   // a selected connection being in a valid format or not.
-  const { rawSelectedConnection: baseUrl } = useSelectedConnection();
-  const url = new URL(`/subgraph`, baseUrl).toString();
+  if (!validatedSelectedConnection.isValid) return null;
+
+  const url = new URL(`/subgraph`, validatedSelectedConnection.url).toString();
 
   return (
     <div className="flex w-full max-w-md items-center space-x-2">
--- apps/ensadmin/src/app/api/subgraph/page.tsx
+++ apps/ensadmin/src/app/api/subgraph/page.tsx
@@ -4,10 +4,21 @@ import { SubgraphGraphiQLEditor } from "@/components/graphiql-editor";
 import { useSelectedConnection } from "@/hooks/active/use-selected-connection";
 
 export default function SubgraphGraphQLPage() {
+  const { validatedSelectedConnection } = useSelectedConnection();
+
   // TODO: we need a broader refactor to recognize the difference between
   // a selected connection being in a valid format or not.
-  const { rawSelectedConnection: baseUrl } = useSelectedConnection();
-  const url = new URL(`/subgraph`, baseUrl).toString();
+  if (!validatedSelectedConnection.isValid) {
+    return (
+      <div className="flex w-full max-w-md items-center space-x-2">
+        <span className="font-mono text-xs select-none text-red-500">
+          Invalid connection URL: {validatedSelectedConnection.error}
+        </span>
+      </div>
+    );
+  }
+
+  const url = new URL(`/subgraph`, validatedSelectedConnection.url).toString();
 
   return <SubgraphGraphiQLEditor url={url} />;
 }
