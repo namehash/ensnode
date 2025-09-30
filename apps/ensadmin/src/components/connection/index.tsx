82149bd77131e15f5085b6a51470afb2565e078b
y3drk <ziebura.jedrzej@gmail.com>
2025-09-30 10:43:21 +0200

    TODO removal

--- apps/ensadmin/src/components/connection/index.tsx
+++ apps/ensadmin/src/components/connection/index.tsx
@@ -6,8 +6,6 @@ import { useENSIndexerConfig } from "@ensnode/ensnode-react";
 //TODO: Would appreciate advice about the naming convention here,
 // since @/app/connection/page.tsx already has a component named "Connection"
 export default function ConnectionInfo() {
-  //TODO: Sometime ago we talked about provider/context that would call useENSIndexerConfig & useIndexingStatus "globally"
-  // and then allow all other components to use it. I assume we still want that long term but it exceeds the scope of this PR?
   const ensIndexerConfigQuery = useENSIndexerConfig();
 
   if (ensIndexerConfigQuery.isError) {
