f48513535b3093116fa1a5480133cdc32a740f04
y3drk <ziebura.jedrzej@gmail.com>
2025-10-03 16:29:46 +0200

    Linting (3)

--- apps/ensadmin/src/components/recent-registrations/components.tsx
+++ apps/ensadmin/src/components/recent-registrations/components.tsx
@@ -1,22 +1,23 @@
 "use client";
 
-import type { ENSNamespaceId } from "@ensnode/datasources";
-import {
-    ENSIndexerOverallIndexingStatus,
-    type ENSIndexerPublicConfig, OverallIndexingStatusId,
-    OverallIndexingStatusIds,
-} from "@ensnode/ensnode-sdk";
-import { useEffect, useState } from "react";
 import { ErrorInfo, ErrorInfoProps } from "@/components/error-info";
+import { RegistrationCard } from "@/components/recent-registrations/registration-card";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useRawConnectionUrlParam } from "@/hooks/use-connection-url-param";
+import { cn } from "@/lib/utils";
+import type { ENSNamespaceId } from "@ensnode/datasources";
+import {
+  ENSIndexerOverallIndexingStatus,
+  type ENSIndexerPublicConfig,
+  OverallIndexingStatusId,
+  OverallIndexingStatusIds,
+} from "@ensnode/ensnode-sdk";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import { useRecentRegistrations } from "./hooks";
-import {Badge} from "@/components/ui/badge";
-import {cn} from "@/lib/utils";
-import {RegistrationCard} from "@/components/recent-registrations/registration-card";
-import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 /**
  * Max number of latest registrations to display
@@ -26,7 +27,10 @@ const DEFAULT_MAX_ROWS = 25;
 /**
  * Omnichain indexing statuses that allow the display of registrations.
  */
-const SUPPORTED_OMNICHAIN_INDEXING_STATUSES: OverallIndexingStatusId[] = [OverallIndexingStatusIds.Following, OverallIndexingStatusIds.Completed]
+const SUPPORTED_OMNICHAIN_INDEXING_STATUSES: OverallIndexingStatusId[] = [
+  OverallIndexingStatusIds.Following,
+  OverallIndexingStatusIds.Completed,
+];
 
 /**
  * RecentRegistrations display variations:
@@ -93,7 +97,11 @@ export function RecentRegistrations({
   }
 
   if (!SUPPORTED_OMNICHAIN_INDEXING_STATUSES.includes(indexingStatus.overallStatus)) {
-    return <UnsupportedOmnichainIndexingStatusMessage overallOmnichainIndexingStatus={indexingStatus.overallStatus} />;
+    return (
+      <UnsupportedOmnichainIndexingStatusMessage
+        overallOmnichainIndexingStatus={indexingStatus.overallStatus}
+      />
+    );
   }
 
   const { ensNodePublicUrl: ensNodeUrl, namespace: namespaceId } = ensIndexerConfig;
@@ -153,15 +161,18 @@ function RegistrationsList({ ensNodeUrl, namespaceId, maxRecords }: Registration
   }
 
   return (
-          <div ref={animationParent} className="w-full h-fit box-border flex flex-col justify-start items-center gap-3">
-              {recentRegistrationsQuery.data?.map((registration) => (
-                  <RegistrationCard
-                      key={registration.name}
-                      registration={registration}
-                      namespaceId={namespaceId}
-                  />
-              ))}
-          </div>
+    <div
+      ref={animationParent}
+      className="w-full h-fit box-border flex flex-col justify-start items-center gap-3"
+    >
+      {recentRegistrationsQuery.data?.map((registration) => (
+        <RegistrationCard
+          key={registration.name}
+          registration={registration}
+          namespaceId={namespaceId}
+        />
+      ))}
+    </div>
   );
 }
 
@@ -181,11 +192,11 @@ function RegistrationsListLoading({ rowCount }: RegistrationLoadingProps) {
 function RecentRegistrationsLoading({ rowCount }: RegistrationLoadingProps) {
   return (
     <Card>
-        <CardHeader>
-            <CardTitle className="flex justify-between items-center">
-                <span>Latest indexed registrations</span>
-            </CardTitle>
-        </CardHeader>
+      <CardHeader>
+        <CardTitle className="flex justify-between items-center">
+          <span>Latest indexed registrations</span>
+        </CardTitle>
+      </CardHeader>
       <CardContent className="max-sm:p-3 max-sm:pt-0">
         <RegistrationsListLoading rowCount={rowCount} />
       </CardContent>
@@ -194,48 +205,64 @@ function RecentRegistrationsLoading({ rowCount }: RegistrationLoadingProps) {
 }
 
 interface UnsupportedOmnichainIndexingStatusMessageProps {
-    overallOmnichainIndexingStatus: OverallIndexingStatusId;
+  overallOmnichainIndexingStatus: OverallIndexingStatusId;
 }
 
-function UnsupportedOmnichainIndexingStatusMessage({overallOmnichainIndexingStatus} : UnsupportedOmnichainIndexingStatusMessageProps) {
+function UnsupportedOmnichainIndexingStatusMessage({
+  overallOmnichainIndexingStatus,
+}: UnsupportedOmnichainIndexingStatusMessageProps) {
   const { retainCurrentRawConnectionUrlParam } = useRawConnectionUrlParam();
 
   return (
     <Card className="w-full">
       <CardHeader className="sm:pb-4 max-sm:p-3">
-          {/*TODO: the wording for the indexer-error is awkward, advice appreciated*/}
-        <CardTitle>{overallOmnichainIndexingStatus === OverallIndexingStatusIds.IndexerError ? "Please investigate your ENSNode instance" : "Please wait for indexing to advance"}</CardTitle>
+        {/*TODO: the wording for the indexer-error is awkward, advice appreciated*/}
+        <CardTitle>
+          {overallOmnichainIndexingStatus === OverallIndexingStatusIds.IndexerError
+            ? "Please investigate your ENSNode instance"
+            : "Please wait for indexing to advance"}
+        </CardTitle>
       </CardHeader>
       <CardContent className="flex flex-col justify-start items-start gap-4 sm:gap-3">
-          <div className="flex flex-row flex-nowrap justify-start items-center gap-2">
-              <p>Current overall omnichain indexing status:</p>
-              <Badge
-                  className={cn(
-                      "uppercase text-xs leading-none",
-                      overallOmnichainIndexingStatus === OverallIndexingStatusIds.IndexerError && "bg-red-600 text-white",
-                  )}
-                  title={`Current overall omnichain indexing status: ${overallOmnichainIndexingStatus}`}
-              >
-                  {overallOmnichainIndexingStatus === OverallIndexingStatusIds.IndexerError
-                      ? "Indexer Error"
-                      : overallOmnichainIndexingStatus}
-              </Badge>
-          </div>
-          {overallOmnichainIndexingStatus === OverallIndexingStatusIds.IndexerError ?
-              <p> It appears that the indexing of new blocks has been interrupted. API requests to this ENSNode
-                  should continue working successfully but may serve data that isn't updated to the latest
-                  onchain state.</p> : <p>
-                  The latest indexed registrations will be available once the omnichain indexing status is{" "}
-                  {SUPPORTED_OMNICHAIN_INDEXING_STATUSES.map((supportedOmnichainIndexingStatus, idx) =>
-                      <>
-                          <span className="font-mono bg-muted p-1 rounded-md">{supportedOmnichainIndexingStatus}</span>
-                          {idx < SUPPORTED_OMNICHAIN_INDEXING_STATUSES.length - 1 && " or "}
-                      </>
-                  )}.
-              </p>}
-          <Button asChild variant="default">
-              <Link href={retainCurrentRawConnectionUrlParam("/status")}>Check status</Link>
-          </Button>
+        <div className="flex flex-row flex-nowrap justify-start items-center gap-2">
+          <p>Current overall omnichain indexing status:</p>
+          <Badge
+            className={cn(
+              "uppercase text-xs leading-none",
+              overallOmnichainIndexingStatus === OverallIndexingStatusIds.IndexerError &&
+                "bg-red-600 text-white",
+            )}
+            title={`Current overall omnichain indexing status: ${overallOmnichainIndexingStatus}`}
+          >
+            {overallOmnichainIndexingStatus === OverallIndexingStatusIds.IndexerError
+              ? "Indexer Error"
+              : overallOmnichainIndexingStatus}
+          </Badge>
+        </div>
+        {overallOmnichainIndexingStatus === OverallIndexingStatusIds.IndexerError ? (
+          <p>
+            {" "}
+            It appears that the indexing of new blocks has been interrupted. API requests to this
+            ENSNode should continue working successfully but may serve data that isn't updated to
+            the latest onchain state.
+          </p>
+        ) : (
+          <p>
+            The latest indexed registrations will be available once the omnichain indexing status is{" "}
+            {SUPPORTED_OMNICHAIN_INDEXING_STATUSES.map((supportedOmnichainIndexingStatus, idx) => (
+              <>
+                <span className="font-mono bg-muted p-1 rounded-md">
+                  {supportedOmnichainIndexingStatus}
+                </span>
+                {idx < SUPPORTED_OMNICHAIN_INDEXING_STATUSES.length - 1 && " or "}
+              </>
+            ))}
+            .
+          </p>
+        )}
+        <Button asChild variant="default">
+          <Link href={retainCurrentRawConnectionUrlParam("/status")}>Check status</Link>
+        </Button>
       </CardContent>
     </Card>
   );
--- apps/ensadmin/src/components/recent-registrations/registration-card.tsx
+++ apps/ensadmin/src/components/recent-registrations/registration-card.tsx
@@ -1,53 +1,58 @@
-import type {Registration} from "@/components/recent-registrations/types";
-import type {ENSNamespaceId} from "@ensnode/datasources";
-import {NameDisplay, NameLink} from "@/components/identity/utils";
-import {Duration, RelativeTime} from "@/components/datetime-utils";
-import {Identity} from "@/components/identity";
-import {PropsWithChildren} from "react";
-import {cn} from "@/lib/utils";
+import { Duration, RelativeTime } from "@/components/datetime-utils";
+import { Identity } from "@/components/identity";
+import { NameDisplay, NameLink } from "@/components/identity/utils";
+import type { Registration } from "@/components/recent-registrations/types";
+import { cn } from "@/lib/utils";
+import type { ENSNamespaceId } from "@ensnode/datasources";
+import { PropsWithChildren } from "react";
 
 export interface RegistrationCardProps {
-    registration: Registration;
-    namespaceId: ENSNamespaceId;
+  registration: Registration;
+  namespaceId: ENSNamespaceId;
 }
 
 /**
  * Displays the data of a single Registration
  */
 
-export function RegistrationCard({registration, namespaceId}: RegistrationCardProps) {
-    return <div
-        className="w-full min-h-[80px] box-border flex flex-row max-lg:flex-wrap flex-nowrap justify-between items-center max-lg:gap-3 rounded-xl border p-3 text-sm">
-        <RegistrationCardElement elementName="Name" className="w-[45%] min-w-[200px]">
-            <div className="w-full overflow-x-auto">
-                <NameLink
-                    name={registration.name}
-                    className="inline-flex items-center gap-2 text-blue-600 hover:underline"
-                >
-                    <NameDisplay name={registration.name}/>
-                </NameLink>
-            </div>
-        </RegistrationCardElement>
-        <RegistrationCardElement elementName="Registered" className="w-[15%] min-w-[100px]">
-            <RelativeTime timestamp={registration.registeredAt} tooltipPosition="top"/>
-        </RegistrationCardElement>
-        <RegistrationCardElement elementName="Duration" className="w-[10%]  min-w-[100px]">
-            <Duration beginsAt={registration.registeredAt} endsAt={registration.expiresAt}/>
-        </RegistrationCardElement>
-        <RegistrationCardElement elementName="Owner" className="w-1/5 overflow-x-auto min-w-[150px]">
-            <Identity address={registration.owner} namespaceId={namespaceId} showAvatar={true}/>
-        </RegistrationCardElement>
+export function RegistrationCard({ registration, namespaceId }: RegistrationCardProps) {
+  return (
+    <div className="w-full min-h-[80px] box-border flex flex-row max-lg:flex-wrap flex-nowrap justify-between items-center max-lg:gap-3 rounded-xl border p-3 text-sm">
+      <RegistrationCardElement elementName="Name" className="w-[45%] min-w-[200px]">
+        <div className="w-full overflow-x-auto">
+          <NameLink
+            name={registration.name}
+            className="inline-flex items-center gap-2 text-blue-600 hover:underline"
+          >
+            <NameDisplay name={registration.name} />
+          </NameLink>
+        </div>
+      </RegistrationCardElement>
+      <RegistrationCardElement elementName="Registered" className="w-[15%] min-w-[100px]">
+        <RelativeTime timestamp={registration.registeredAt} tooltipPosition="top" />
+      </RegistrationCardElement>
+      <RegistrationCardElement elementName="Duration" className="w-[10%]  min-w-[100px]">
+        <Duration beginsAt={registration.registeredAt} endsAt={registration.expiresAt} />
+      </RegistrationCardElement>
+      <RegistrationCardElement elementName="Owner" className="w-1/5 overflow-x-auto min-w-[150px]">
+        <Identity address={registration.owner} namespaceId={namespaceId} showAvatar={true} />
+      </RegistrationCardElement>
     </div>
+  );
 }
 
 interface RegistrationCardElementProps {
-    elementName: string;
-    className?: string;
+  elementName: string;
+  className?: string;
 }
 
-const RegistrationCardElement =
-({elementName, className, children}: PropsWithChildren<RegistrationCardElementProps>) =>
-    <div className={cn("flex flex-col flex-nowrap justify-start items-start", className)}>
-        <p className="text-muted-foreground text-sm leading-normal font-normal">{elementName}</p>
-        {children}
-    </div>
+const RegistrationCardElement = ({
+  elementName,
+  className,
+  children,
+}: PropsWithChildren<RegistrationCardElementProps>) => (
+  <div className={cn("flex flex-col flex-nowrap justify-start items-start", className)}>
+    <p className="text-muted-foreground text-sm leading-normal font-normal">{elementName}</p>
+    {children}
+  </div>
+);
