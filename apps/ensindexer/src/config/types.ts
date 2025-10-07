ac6fb0e97c8e05fed846cd8e6a8799c60e3e0bab
shrugs <mattgcondon@gmail.com>
2025-10-07 15:51:38 -0500

    add test case for rpc url overloading

--- apps/ensindexer/src/config/types.ts
+++ apps/ensindexer/src/config/types.ts
@@ -241,28 +241,27 @@ export type RpcConfigEnvironment = string;
 /**
  * Represents the raw, unvalidated environment variables for the ENSIndexer application.
  *
- * Keys correspond to the environment variable names, and all values are
- * strings or undefined, reflecting their state in `process.env`.
- * This interface is intended to be the source type which then gets
+ * Keys correspond to the environment variable names, and all values are optional strings, reflecting
+ * their state in `process.env`. This interface is intended to be the source type which then gets
  * mapped/parsed into a structured configuration object like `ENSIndexerConfig`.
  */
 export interface ENSIndexerEnvironment {
-  PORT: string | undefined;
-  DATABASE_SCHEMA: string | undefined;
-  DATABASE_URL: string | undefined;
-  NAMESPACE: string | undefined;
-  PLUGINS: string | undefined;
-  ENSRAINBOW_URL: string | undefined;
-  LABEL_SET_ID: string | undefined;
-  LABEL_SET_VERSION: string | undefined;
-  ENSNODE_PUBLIC_URL: string | undefined;
-  ENSINDEXER_URL: string | undefined;
-  ENSADMIN_URL: string | undefined;
-  START_BLOCK: string | undefined;
-  END_BLOCK: string | undefined;
-  SUBGRAPH_COMPAT: string | undefined;
+  PORT?: string;
+  DATABASE_SCHEMA?: string;
+  DATABASE_URL?: string;
+  NAMESPACE?: string;
+  PLUGINS?: string;
+  ENSRAINBOW_URL?: string;
+  LABEL_SET_ID?: string;
+  LABEL_SET_VERSION?: string;
+  ENSNODE_PUBLIC_URL?: string;
+  ENSINDEXER_URL?: string;
+  ENSADMIN_URL?: string;
+  START_BLOCK?: string;
+  END_BLOCK?: string;
+  SUBGRAPH_COMPAT?: string;
 
   [x: `RPC_URL_${number}`]: string | undefined;
-  ALCHEMY_API_KEY: string | undefined;
-  DRPC_API_KEY: string | undefined;
+  ALCHEMY_API_KEY?: string;
+  DRPC_API_KEY?: string;
 }
