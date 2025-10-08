fd3f44af62e66e8d2877e41dfa85677e4fb00bd9
shrugs <mattgcondon@gmail.com>
2025-10-08 11:48:43 -0500

    fix: allow subgraph compatible for ens-test-env

--- packages/ensnode-sdk/src/ensindexer/config/helpers.ts
+++ packages/ensnode-sdk/src/ensindexer/config/helpers.ts
@@ -1,3 +1,4 @@
+import { ENSNamespaceIds } from "@ensnode/datasources";
 import { type ENSIndexerPublicConfig, PluginName } from "./types";
 
 /**
@@ -8,6 +9,7 @@ import { type ENSIndexerPublicConfig, PluginName } from "./types";
 export function isSubgraphCompatible(
   config: Pick<
     ENSIndexerPublicConfig,
+    | "namespace"
     | "plugins"
     | "healReverseAddresses"
     | "indexAdditionalResolverRecords"
@@ -27,9 +29,16 @@ export function isSubgraphCompatible(
     !config.indexAdditionalResolverRecords &&
     !config.replaceUnnormalized;
 
-  // 5. label set id must be "subgraph" and version must be 0
-  const labelSetIsSubgraphCompatible =
+  const isSubgraphLabelSet =
     config.labelSet.labelSetId === "subgraph" && config.labelSet.labelSetVersion === 0;
+
+  const isEnsTestEnvLabelSet =
+    config.labelSet.labelSetId === "ens-test-env" && config.labelSet.labelSetVersion === 0;
+
+  // 5. label set id must be "subgraph" and version must be 0 in non-ens-test-env namespaces, otherwise
+  // ens-test-env labelset is allowed
+  const labelSetIsSubgraphCompatible =
+    isSubgraphLabelSet || (config.namespace === ENSNamespaceIds.EnsTestEnv && isEnsTestEnvLabelSet);
 
   return (
     onlySubgraphPluginActivated &&
