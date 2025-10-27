ec1e4ef47c6faa6c421f9130a630266735fc8211
Jamie Barton <jamie@notrab.dev>
2025-10-27 09:36:06 +0000

    export statci

--- apps/ensadmin/next.config.ts
+++ apps/ensadmin/next.config.ts
@@ -1,39 +1,9 @@
 import type { NextConfig } from "next";
 
-let output: NextConfig["output"];
-
-if (process.env.NEXT_BUILD_OUTPUT_STANDALONE === "true") {
-  output = "standalone";
-}
-
 const nextConfig: NextConfig = {
-  // Docker builds require the standalone output
-  // Vercel builds require the default (`undefined`) output
-  output,
+  output: "export",
   // Enable source maps in production
   productionBrowserSourceMaps: true,
-  async redirects() {
-    return [
-      {
-        // Provide backward compatibility for ENSNode services.
-        // The older ones would redirect their homepage to ENSAdmin `/about`
-        // which was discontinued in the application router.
-        source: "/about",
-        destination: "/",
-        permanent: true,
-      },
-      {
-        source: "/gql/subgraph-compat",
-        destination: "/api/subgraph",
-        permanent: true,
-      },
-      {
-        source: "/gql/ponder",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
