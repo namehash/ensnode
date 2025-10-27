133de83125b20e322dc59e6c9961795d1f3e441d
Jamie Barton <jamie@notrab.dev>
2025-10-27 15:36:25 +0000

    add requested comment

--- apps/ensadmin/next.config.ts
+++ apps/ensadmin/next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Use Single Page App mode only for ENSAdmin
   output: "export",
   // Enable source maps in production
   productionBrowserSourceMaps: true,
