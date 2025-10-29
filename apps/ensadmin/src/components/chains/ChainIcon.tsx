2d71b6ec19d0d29e14205cc3f5119db17dc42e93
shrugs <mattgcondon@gmail.com>
2025-10-29 13:22:40 -0500

    fix: attempt simpler chainicon lookup

--- apps/ensadmin/src/components/chains/ChainIcon.tsx
+++ apps/ensadmin/src/components/chains/ChainIcon.tsx
@@ -41,34 +41,34 @@ export interface ChainIconProps {
  * Mapping of chain id to chain icon.
  * Chain id standards are organized by the Ethereum Community @ https://github.com/ethereum-lists/chains
  */
-const chainIcons = new Map<number, React.ComponentType<React.SVGProps<SVGSVGElement>>>([
+const chainIcons = {
   // mainnet
-  [mainnet.id, EthereumIcon],
-  [base.id, BaseIcon],
-  [linea.id, LineaIcon],
-  [optimism.id, OptimismIcon],
-  [arbitrum.id, ArbitrumIcon],
-  [scroll.id, ScrollIcon],
+  [mainnet.id]: EthereumIcon,
+  [base.id]: BaseIcon,
+  [linea.id]: LineaIcon,
+  [optimism.id]: OptimismIcon,
+  [arbitrum.id]: ArbitrumIcon,
+  [scroll.id]: ScrollIcon,
 
   // sepolia
-  [sepolia.id, EthereumTestnetIcon],
-  [baseSepolia.id, BaseTestnetIcon],
-  [lineaSepolia.id, LineaTestnetIcon],
-  [optimismSepolia.id, OptimismTestnetIcon],
-  [arbitrumSepolia.id, ArbitrumTestnetIcon],
-  [scrollSepolia.id, ScrollTestnetIcon],
+  [sepolia.id]: EthereumTestnetIcon,
+  [baseSepolia.id]: BaseTestnetIcon,
+  [lineaSepolia.id]: LineaTestnetIcon,
+  [optimismSepolia.id]: OptimismTestnetIcon,
+  [arbitrumSepolia.id]: ArbitrumTestnetIcon,
+  [scrollSepolia.id]: ScrollTestnetIcon,
 
   // holesky
-  [holesky.id, EthereumTestnetIcon],
+  [holesky.id]: EthereumTestnetIcon,
 
   // ens-test-env
-  [ensTestEnvL1Chain.id, EthereumLocalIcon],
-]);
+  [ensTestEnvL1Chain.id]: EthereumLocalIcon,
+};
 
 /**
  * Renders an icon for the provided chain ID.
  */
 export function ChainIcon({ chainId, width = 20, height = 20 }: ChainIconProps) {
-  const Icon = chainIcons.get(chainId) || UnrecognizedChainIcon;
+  const Icon = chainIcons[chainId] || UnrecognizedChainIcon;
   return <Icon width={width} height={height} />;
 }
