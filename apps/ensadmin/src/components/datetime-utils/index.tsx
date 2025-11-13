393626e7f41ee9dd77996c7f01ef3d7cce2ac322
Jamie Barton <jamie@notrab.dev>
2025-11-13 20:58:54 +0000

    update relative time when seconds

--- apps/ensadmin/src/components/datetime-utils/index.tsx
+++ apps/ensadmin/src/components/datetime-utils/index.tsx
@@ -92,9 +92,18 @@ export function RelativeTime({
   const [relativeTime, setRelativeTime] = useState<string>("");
 
   useEffect(() => {
-    setRelativeTime(
-      formatRelativeTime(timestamp, enforcePast, includeSeconds, conciseFormatting, relativeTo),
-    );
+    const updateTime = () => {
+      setRelativeTime(
+        formatRelativeTime(timestamp, enforcePast, includeSeconds, conciseFormatting, relativeTo),
+      );
+    };
+
+    updateTime();
+
+    if (includeSeconds) {
+      const interval = setInterval(updateTime, 1000);
+      return () => clearInterval(interval);
+    }
   }, [timestamp, conciseFormatting, enforcePast, includeSeconds, relativeTo]);
 
   return (
