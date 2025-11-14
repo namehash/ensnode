a35b628fba1dd32a753be4073f30178493f0f9b8
shrugs <mattgcondon@gmail.com>
2025-11-13 18:40:49 -0600

    checkpoint

--- apps/ensindexer/src/plugins/ensv2/handlers/ENSv1Registry.ts
+++ apps/ensindexer/src/plugins/ensv2/handlers/ENSv1Registry.ts
@@ -5,6 +5,8 @@ import schema from "ponder:schema";
 import { type Address, isAddressEqual, zeroAddress, zeroHash } from "viem";
 
 import {
+  ADDR_REVERSE_NODE,
+  getENSRootChainId,
   getRootRegistry,
   getRootRegistryId,
   type LabelHash,
@@ -16,118 +18,135 @@ import {
   PluginName,
 } from "@ensnode/ensnode-sdk";
 
-import { ensureAccount } from "@/lib/ensv2/account-db-helpers";
 import { materializeDomainOwner } from "@/lib/ensv2/domain-db-helpers";
-import { ensureUnknownLabel } from "@/lib/ensv2/label-db-helpers";
+import { ensureLabel, ensureUnknownLabel } from "@/lib/ensv2/label-db-helpers";
+import { healAddrReverseSubnameLabel } from "@/lib/heal-addr-reverse-subname-label";
 import { namespaceContract } from "@/lib/plugin-helpers";
 import type { EventWithArgs } from "@/lib/ponder-helpers";
 import { nodeIsMigrated } from "@/lib/protocol-acceleration/registry-migration-status";
 
 const pluginName = PluginName.ENSv2;
 
-async function handleNewOwner({
-  context,
-  event,
-}: {
-  context: Context;
-  event: EventWithArgs<{
-    // NOTE: `node` event arg represents a `Node` that is the _parent_ of the node the NewOwner event is about
-    node: Node;
-    // NOTE: `label` event arg represents a `LabelHash` for the sub-node under `node`
-    label: LabelHash;
-    owner: Address;
-  }>;
-}) {
-  const { label: labelHash, node: parentNode, owner } = event.args;
-
-  // if someone mints a node to the zero address, nothing happens in the Registry, so no-op
-  if (isAddressEqual(zeroAddress, owner)) return;
-
-  // this is either a NEW Domain OR the owner of the parent changing the owner of the child
-
-  const node = makeSubdomainNode(labelHash, parentNode);
-  const domainId = makeENSv1DomainId(node);
-  const registryId =
-    parentNode === zeroHash
-      ? getRootRegistryId(config.namespace)
-      : makeImplicitRegistryId(parentNode);
-
-  // TODO: import label healing logic from subgraph plugin
-
-  await ensureUnknownLabel(context, labelHash);
-  await context.db
-    .insert(schema.domain)
-    .values({
-      id: domainId,
-      labelHash,
-      registryId,
-    })
-    .onConflictDoNothing();
-
-  // materialize domain owner
-  // NOTE: despite Domain.ownerId being materialized from other sources of truth (i.e. Registrars
-  // like BaseRegistrars & NameWrapper) it's ok to always set it here because the Registrar-emitted
-  // events occur _after_ the Registry events. So when a name is wrapped, for example, the Registry's
-  // owner changes to that of the NameWrapper but then the NameWrapper emits NameWrapped, and this
-  // indexing code re-materializes the Domain.ownerId to the NameWraper-emitted value.
-  await materializeDomainOwner(context, domainId, owner);
-}
-
-async function handleNewResolver({
-  context,
-  event,
-}: {
-  context: Context;
-  event: EventWithArgs<{ node: Node; resolver: Address }>;
-}) {
-  const { node, resolver: address } = event.args;
-
-  const domainId = makeENSv1DomainId(node);
-
-  // update domain's resolver
-  const isDeletion = isAddressEqual(address, zeroAddress);
-  if (isDeletion) {
-    await context.db.update(schema.domain, { id: domainId }).set({ resolverId: null });
-  } else {
-    const resolverId = makeResolverId({ chainId: context.chain.id, address: address });
-    await context.db.update(schema.domain, { id: domainId }).set({ resolverId });
-  }
-}
-
-export async function handleTransfer({
-  context,
-  event,
-}: {
-  context: Context;
-  event: EventWithArgs<{ node: Node; owner: Address }>;
-}) {
-  const { node, owner } = event.args;
-
-  // ENSv2 model does not include root node, no-op
-  if (node === zeroHash) return;
-
-  const domainId = makeENSv1DomainId(node);
-
-  const isDeletion = isAddressEqual(zeroAddress, owner);
-  if (isDeletion) {
-    await context.db.delete(schema.domain, { id: domainId });
-    return;
-  }
-
-  // TODO: if owner is special registrar, ignore
-
-  // ensure owner account
-  await ensureAccount(context, owner);
-
-  // update owner
-  await context.db.update(schema.domain, { id: domainId }).set({ ownerId: owner });
-}
-
 /**
  * Handler functions for ENSv1 Regsitry contracts.
  * - piggybacks Protocol Resolution plugin's Node Migration status
  */
 export default function () {
+  /**
+   * Registry#NewOwner is either a new Domain OR the owner of the parent changing the owner of the child.
+   */
+  async function handleNewOwner({
+    context,
+    event,
+  }: {
+    context: Context;
+    event: EventWithArgs<{
+      // NOTE: `node` event arg represents a `Node` that is the _parent_ of the node the NewOwner event is about
+      node: Node;
+      // NOTE: `label` event arg represents a `LabelHash` for the sub-node under `node`
+      label: LabelHash;
+      owner: Address;
+    }>;
+  }) {
+    const { label: labelHash, node: parentNode, owner } = event.args;
+
+    // if someone mints a node to the zero address, nothing happens in the Registry, so no-op
+    if (isAddressEqual(zeroAddress, owner)) return;
+
+    const node = makeSubdomainNode(labelHash, parentNode);
+    const domainId = makeENSv1DomainId(node);
+    const registryId =
+      parentNode === zeroHash
+        ? getRootRegistryId(config.namespace)
+        : makeImplicitRegistryId(parentNode);
+
+    // If this is a direct subname of addr.reverse, we have 100% on-chain label discovery.
+    //
+    // Note: Per ENSIP-19, only the ENS Root chain may record primary names under the `addr.reverse`
+    // subname. Also per ENSIP-19 no Reverse Names need exist in (shadow)Registries on non-root
+    // chains, so we explicitly only support Root chain addr.reverse-based Reverse Names: ENSIP-19
+    // CoinType-specific Reverse Names (ex: [address].[coinType].reverse) don't actually exist in
+    // the ENS Registry: wildcard resolution is used, so this NewOwner event will never be emitted
+    // with a domain created as a child of a Coin-Type specific Reverse Node (ex: [coinType].reverse).
+    if (
+      parentNode === ADDR_REVERSE_NODE &&
+      context.chain.id === getENSRootChainId(config.namespace)
+    ) {
+      const label = await healAddrReverseSubnameLabel(context, event, labelHash);
+      await ensureLabel(context, label);
+    } else {
+      await ensureUnknownLabel(context, labelHash);
+    }
+
+    // upsert domain
+    await context.db
+      .insert(schema.domain)
+      .values({
+        id: domainId,
+        labelHash,
+        registryId,
+      })
+      .onConflictDoNothing();
+
+    // materialize domain owner
+    // NOTE: despite Domain.ownerId being materialized from other sources of truth (i.e. Registrars
+    // like BaseRegistrars & NameWrapper) it's ok to always set it here because the Registrar-emitted
+    // events occur _after_ the Registry events. So when a name is registered, for example, the Registry's
+    // owner changes to that of the NameWrapper but then the NameWrapper emits NameWrapped, and this
+    // indexing code re-materializes the Domain.ownerId to the NameWraper-emitted value.
+    await materializeDomainOwner(context, domainId, owner);
+  }
+
+  async function handleNewResolver({
+    context,
+    event,
+  }: {
+    context: Context;
+    event: EventWithArgs<{ node: Node; resolver: Address }>;
+  }) {
+    const { node, resolver: address } = event.args;
+
+    const domainId = makeENSv1DomainId(node);
+
+    // update domain's resolver
+    const isDeletion = isAddressEqual(address, zeroAddress);
+    if (isDeletion) {
+      await context.db.update(schema.domain, { id: domainId }).set({ resolverId: null });
+    } else {
+      const resolverId = makeResolverId({ chainId: context.chain.id, address: address });
+      await context.db.update(schema.domain, { id: domainId }).set({ resolverId });
+    }
+  }
+
+  async function handleTransfer({
+    context,
+    event,
+  }: {
+    context: Context;
+    event: EventWithArgs<{ node: Node; owner: Address }>;
+  }) {
+    const { node, owner } = event.args;
+
+    // ENSv2 model does not include root node, no-op
+    if (node === zeroHash) return;
+
+    const domainId = makeENSv1DomainId(node);
+
+    const isDeletion = isAddressEqual(zeroAddress, owner);
+    if (isDeletion) {
+      await context.db.delete(schema.domain, { id: domainId });
+      return;
+    }
+
+    // materialize domain owner
+    // NOTE: despite Domain.ownerId being materialized from other sources of truth (i.e. Registrars
+    // like BaseRegistrars & NameWrapper) it's ok to always set it here because the Registrar-emitted
+    // events occur _after_ the Registry events. So when a name is wrapped, for example, the Registry's
+    // owner changes to that of the NameWrapper but then the NameWrapper emits NameWrapped, and this
+    // indexing code re-materializes the Domain.ownerId to the NameWraper-emitted value.
+    await materializeDomainOwner(context, domainId, owner);
+  }
+
   /**
    * Sets up the ENSv2 Root Registry
    */
@@ -177,6 +196,10 @@ export default function () {
     },
   );
 
+  /**
+   * Handles Registry#Transfer for:
+   * - ENS Root Chain's ENSv1RegistryOld
+   */
   ponder.on(
     namespaceContract(pluginName, "ENSv1RegistryOld:Transfer"),
     async ({ context, event }) => {
