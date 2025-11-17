2faaa5ff25f74a5852a9fd93bd6e522e0a263d32
shrugs <mattgcondon@gmail.com>
2025-11-17 12:03:00 -0600

    ens-test-env default urls

--- packages/datasources/src/lib/chains.ts
+++ packages/datasources/src/lib/chains.ts
@@ -16,10 +16,12 @@ export const ensTestEnvL1Chain = {
   ...localhost,
   id: l1ChainId,
   name: "ens-test-env L1",
+  rpcUrls: { default: { http: ["http://localhost:8545"] } },
 } satisfies Chain;
 
 export const ensTestEnvL2Chain = {
   ...localhost,
   id: l2ChainId,
   name: "ens-test-env L2",
+  rpcUrls: { default: { http: ["http://localhost:8546"] } },
 } satisfies Chain;
