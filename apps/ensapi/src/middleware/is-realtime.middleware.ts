81ad17d5f66a8c1eb60368c435650fc8d0eb54fa
lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>
2025-11-25 17:17:39 +0400

    refine logging logic

--- apps/ensapi/src/middleware/is-realtime.middleware.ts
+++ apps/ensapi/src/middleware/is-realtime.middleware.ts
@@ -3,10 +3,6 @@ import type { Duration } from "@ensnode/ensnode-sdk";
 import { factory } from "@/lib/hono-factory";
 import { makeLogger } from "@/lib/logger";
 
-let loggedIsRejected = false;
-
-let lastLoggedIsRealtime: boolean | null = null;
-
 /**
  * Type definition for the is realtime middleware context passed to downstream middleware and handlers.
  */
@@ -14,6 +10,9 @@ export type IsRealtimeMiddlewareVariables = { isRealtime: boolean };
 
 export const makeIsRealtimeMiddleware = (scope: string, maxRealtimeDistance: Duration) => {
   const logger = makeLogger(scope);
+
+  let loggedIsRejected = false;
+  let lastLoggedIsRealtime: boolean | null = null;
 
   return factory.createMiddleware(async function isRealtimeMiddleware(c, next) {
     // context must be set by the required middleware
@@ -27,24 +26,29 @@ export const makeIsRealtimeMiddleware = (scope: string, maxRealtimeDistance: Dur
         logger.warn(
           `ENSIndexer is NOT guaranteed to be within ${maxRealtimeDistance} seconds of realtime. Current indexing status has not been successfully fetched by this ENSApi instance yet and is therefore unknown to this ENSApi instance because: ${c.var.indexingStatus.reason}.`,
         );
+        // we log this warning a max of once per `isRealtimeMiddleware` per
+        // ENSApi instance lifecycle since for an ENSApi instance lifecycle,
+        // it's impossible to the indexing status middleware to transition back
+        // to `isRejected` after becoming `isFulfilled`.
         loggedIsRejected = true;
       }
       c.set("isRealtime", false);
       return await next();
     }
 
-    // determine whether we're within an acceptable window to accelerate
+    // determine if we're within the max worst-case distance to qualify as "realtime".
     const isRealtime = c.var.indexingStatus.value.worstCaseDistance <= maxRealtimeDistance;
 
-    if (isRealtime && lastLoggedIsRealtime !== isRealtime) {
-      logger.info(
-        `ENSIndexer is guaranteed to be within ${maxRealtimeDistance} seconds of realtime.`,
-      );
-      lastLoggedIsRealtime = isRealtime;
-    } else if (!isRealtime && lastLoggedIsRealtime !== isRealtime) {
-      logger.warn(
-        `ENSIndexer is NOT guaranteed to be within ${maxRealtimeDistance} seconds of realtime. (Worst Case distance: ${c.var.indexingStatus.value.worstCaseDistance} seconds > ${maxRealtimeDistance} seconds).`,
-      );
+    if (lastLoggedIsRealtime !== isRealtime) {
+      if (isRealtime) {
+        logger.info(
+          `ENSIndexer is guaranteed to be within ${maxRealtimeDistance} seconds of realtime.`,
+        );
+      } else {
+        logger.warn(
+          `ENSIndexer is NOT guaranteed to be within ${maxRealtimeDistance} seconds of realtime. (Worst Case distance: ${c.var.indexingStatus.value.worstCaseDistance} seconds > ${maxRealtimeDistance} seconds).`,
+        );
+      }
       lastLoggedIsRealtime = isRealtime;
     }
 
