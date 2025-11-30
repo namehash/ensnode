00fa26946d49f8dabe163b83f0487237f1f8e5b2
lightwalker.eth <126201998+lightwalker-eth@users.noreply.github.com>
2025-11-30 08:05:30 +0400

    Update ensanalytics-api.test.ts

--- apps/ensapi/src/handlers/ensanalytics-api.test.ts
+++ apps/ensapi/src/handlers/ensanalytics-api.test.ts
@@ -42,8 +42,8 @@ import app from "./ensanalytics-api";
 
 describe("/ensanalytics", () => {
   describe("/referrers", () => {
-    it("returns requested records when cached referrer leaderboard has multiple pages of data", async () => {
-      // Arrange: set `referrerLeaderboardCache` context var
+    it("returns requested records when referrer leaderboard has multiple pages of data", async () => {
+      // Arrange: set `referrerLeaderboard` context var
       vi.mocked(middleware.referrerLeaderboardMiddleware).mockImplementation(async (c, next) => {
         const mockedReferrerLeaderboard = await pReflect(
           Promise.resolve(populatedReferrerLeaderboard),
