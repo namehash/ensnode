4a15ca17c63158c48d02a13cc83c3e13a3e3f31c
Tomasz Kopacki <tomasz@kopacki.net>
2025-12-04 16:36:57 +0100

    refactor(ensnode-sdk): api module
    Split the module into smaller submodules to improve code structure in preparation for adding pagination feature to Registrar Actions API.

---
+++ packages/ensnode-sdk/src/api/shared/pagination/request.ts
@@ -0,0 +1,22 @@
+export const ITEMS_PER_PAGE_DEFAULT = 10;
+
+export const ITEMS_PER_PAGE_MAX = 100;
+
+/**
+ * Request pagination params.
+ */
+export interface RequestPaginationParams {
+  /**
+   * Requested page number (1-indexed)
+   * @invariant Must be a positive integer (>= 1)
+   * @default 1
+   */
+  page?: number;
+
+  /**
+   * Maximum number of items to return per page
+   * @invariant Must be a positive integer (>= 1) and less than or equal to {@link ITEMS_PER_PAGE_MAX}
+   * @default {@link ITEMS_PER_PAGE_DEFAULT}
+   */
+  itemsPerPage?: number;
+}
