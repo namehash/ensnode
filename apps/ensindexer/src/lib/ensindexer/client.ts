623260f8c2f3b21a58824dbd2fec1d865dead0ab
Tomasz Kopacki <tomasz@kopacki.net>
2025-12-17 21:09:23 +0100

    apply pr feedback
    Remove logs from ENSIndexerClient class

--- apps/ensindexer/src/lib/ensindexer/client.ts
+++ apps/ensindexer/src/lib/ensindexer/client.ts
@@ -3,10 +3,6 @@ import type {
   SerializedIndexingStatusResponse,
 } from "@ensnode/ensnode-sdk";
 
-import { makeLogger } from "@/lib/logger";
-
-const logger = makeLogger("ensindexer.client");
-
 export class EnsIndexerClient {
   constructor(private ensIndexerUrl: URL) {}
 
@@ -16,15 +12,10 @@ export class EnsIndexerClient {
    * @throws when the Health endpoint didn't return HTTP Status OK.
    */
   public async health(): Promise<void> {
-    logger.debug("Fetching ENSIndexer Health status");
-
     try {
       await fetch(new URL("/health", this.ensIndexerUrl));
-
-      logger.debug("Fetching ENSIndexer Health status: healthy");
     } catch {
       const errorMessage = "Health endpoint for ENSIndexer is not available yet.";
-      logger.error(errorMessage);
 
       throw new Error(errorMessage);
     }
@@ -37,13 +28,9 @@ export class EnsIndexerClient {
    * @throws error when fetching ENSIndexer Public Config failed
    */
   public async config(): Promise<SerializedENSIndexerPublicConfig> {
-    logger.debug("Fetching ENSIndexer Public Config");
-
     const ensIndexerPublicConfigSerialized = await fetch(
       new URL("/api/config", this.ensIndexerUrl),
     ).then((response) => response.json());
-
-    logger.debug("Fetched ENSIndexer Public Config");
 
     return ensIndexerPublicConfigSerialized as SerializedENSIndexerPublicConfig;
   }
@@ -55,13 +42,9 @@ export class EnsIndexerClient {
    * @throws error when Indexing Status was either not available, or invalid.
    */
   public async indexingStatus(): Promise<SerializedIndexingStatusResponse> {
-    logger.debug("Fetching Indexing Status");
-
     const indexingStatusSerialized = await fetch(
       new URL("/api/indexing-status", this.ensIndexerUrl),
     ).then((response) => response.json());
-
-    logger.debug("Fetched Indexing Status");
 
     return indexingStatusSerialized as SerializedIndexingStatusResponse;
   }
