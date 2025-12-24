c5a02cebe0b71627bdf2c686a17d9acb6427a2f7
shrugs <mattgcondon@gmail.com>
2025-12-24 10:51:52 -0600

    update docs

--- apps/ensindexer/src/lib/managed-names.ts
+++ apps/ensindexer/src/lib/managed-names.ts
@@ -15,21 +15,21 @@ import {
 import { toJson } from "@/lib/json-stringify-with-bigints";
 
 /**
- * Many Registrar contracts within the ENSv1 Ecosystem are relative to a parent Name. For example,
+ * Many contracts within the ENSv1 Ecosystem are relative to a parent Name. For example,
  * the .eth BaseRegistrar (and RegistrarConrollers) manage direct subnames of .eth. As such, they
  * operate on relative Labels, not fully qualified Names. We must know the parent name whose subnames
  * they manage in order to index them correctly.
  *
  * Because we use shared indexing logic for each instance of these contracts (BaseRegistrar,
  * RegistrarControllers, NameWrapper), the concept of "which name is this contract operating in the
- * context of" must be generalizable: this is the Registrar Managed Name.
+ * context of" must be generalizable: this is the contract's 'Managed Name'.
  *
- * Concretely, a .eth RegistrarController will emit a LabelHash indicating a new Registration, but
+ * Concretely, a .eth RegistrarController will emit a _LabelHash_ indicating a new Registration, but
  * correlating that LabelHash with the NameHash of the Name requires knowing the NameHash of the
  * Registrar's Managed Name ('eth' in this case).
  *
  * The NameWrapper contracts are relevant here as well because they include specialized logic for
- * wrapping direct subnames of these Registrar Managed Names.
+ * wrapping direct subnames of these Managed Names.
  */
 
 const ethnamesNameWrapper = getDatasourceContract(
@@ -94,7 +94,11 @@ const CONTRACTS_BY_MANAGED_NAME: Record<Name, AccountId[]> = {
     ),
   ].filter((c) => !!c),
   "linea.eth": [
-    maybeGetDatasourceContract(config.namespace, DatasourceNames.Lineanames, "BaseRegistrar"),
+    maybeGetDatasourceContract(
+      config.namespace, //
+      DatasourceNames.Lineanames,
+      "BaseRegistrar",
+    ),
     maybeGetDatasourceContract(
       config.namespace,
       DatasourceNames.Lineanames,
@@ -115,24 +119,24 @@ const MANAGED_NAME_BY_NAMESPACE: Partial<Record<ENSNamespaceId, Record<Name, Nam
 };
 
 /**
- * Given a `contract`, identify its ManagedName.
+ * Given a `contract`, identify its Managed Name.
  */
 export const getManagedName = (contract: AccountId) => {
   for (const [managedName, contracts] of Object.entries(CONTRACTS_BY_MANAGED_NAME)) {
     const isAnyOfTheContracts = contracts.some((_contract) => accountIdEqual(_contract, contract));
     if (isAnyOfTheContracts) {
-      // override the default Managed Name with namespace-specific version if available
+      // use the namespace-specific Managed Name if specified, otherwise use the default from CONTRACTS_BY_MANAGED_NAME
       const namespaceSpecificManagedName =
         MANAGED_NAME_BY_NAMESPACE[config.namespace]?.[managedName] ?? managedName;
       return namespaceSpecificManagedName as InterpretedName;
     }
   }
 
-  throw new Error(`The following contract ${toJson(contract)} does not map to a Managed Name.`);
+  throw new Error(`The following contract ${toJson(contract)} does not have a Managed Name.`);
 };
 
 /**
- * Determines whether `contract` is the NameWrapper.
+ * Determines whether `contract` is a NameWrapper.
  */
 export function isNameWrapper(contract: AccountId) {
   if (accountIdEqual(ethnamesNameWrapper, contract)) return true;
@@ -141,9 +145,18 @@ export function isNameWrapper(contract: AccountId) {
 }
 
 /**
- * BaseRegistrar-derived Registrars register direct subnames of a Managed Name. As such, the
- * tokens issued by them are keyed by the direct subname's label's labelHash. This is identical for
- * Basenames and Lineanames contracts as well.
+ * Decodes a uint256-encoded-LabelHash (i.e. a tokenId) into a {@link LabelHash}.
+ *
+ * Remember that contracts that operate in the context of a Managed Name frequently store and operate
+ * over _LabelHashes_ that represent a direct subname of a Managed Name. These contracts also frequently
+ * implement ERC721 or ERC1155 to represent ownership of these Names. As such, to construct the
+ * EC721/ERC1155 tokenId, they encode the direct subnames's LabelHash as a uint256.
+ *
+ * This is true for the ENSv1 BaseRegistrar, RegistrarControllers, and NameWrapper, as well as any
+ * contracts forked from from (which includes Basenames' and Lineanames' implementations).
+ *
+ * So, in order to turn the tokenId into a LabelHash, we perform the opposite operation, decoding
+ * from a uint256 into a Hex (of size 32) and cast it as our semantic {@link LabelHash} type.
  *
  * @see https://github.com/ensdomains/ens-contracts/blob/db613bc/contracts/ethregistrar/ETHRegistrarController.sol#L215
  * @see https://github.com/base/basenames/blob/1b5c1ad/src/L2/RegistrarController.sol#L488
