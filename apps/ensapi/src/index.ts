3c4965b860934bac65c9e800a8de63bb35a452f0
Jamie Barton <jamie@notrab.dev>
2026-01-14 20:20:28 +0000

    feat(ensapi): set active servers for openapi

--- apps/ensapi/src/index.ts
+++ apps/ensapi/src/index.ts
@@ -79,7 +79,14 @@ app.get(
         description:
           "APIs for ENS resolution, navigating the ENS nameforest, and metadata about an ENSNode",
       },
-      servers: [{ url: `http://localhost:${config.port}`, description: "Local Development" }],
+      servers: [
+        { url: "https://api.alpha.ensnode.io", description: "ENSNode Alpha (Mainnet)" },
+        {
+          url: "https://api.alpha-sepolia.ensnode.io",
+          description: "ENSNode Alpha (Sepolia Testnet)",
+        },
+        { url: `http://localhost:${config.port}`, description: "Local Development" },
+      ],
       tags: [
         {
           name: "Resolution",
