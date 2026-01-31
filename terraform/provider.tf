87f8e3a8a65da64e39db5df3bb4cb474e8756f3b
Tomasz Kopacki <tomasz@kopacki.net>
2026-01-31 14:56:19 +0100

    Update terraform version to use required TF features for bucket client

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,11 +6,10 @@ terraform {
     }
   }
 
-
   backend "s3" {
-    bucket                      = "ensnode-terraform"
-    key                         = "render-tfstate"
-    region                      = "auto"
+    bucket = "ensnode-terraform"
+    key    = "render-tfstate"
+    region = "auto"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
