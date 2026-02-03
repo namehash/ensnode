7051307cfe6cbd332f0fb13ac8cd2c5fc1f97d12
Tomasz Kopacki <tomasz@kopacki.net>
2026-02-03 22:04:55 +0100

    Export useful types

--- packages/ponder-sdk/src/index.ts
+++ packages/ponder-sdk/src/index.ts
@@ -1,1 +1,4 @@
+export type { BlockNumber, BlockRef } from "./block";
+export type { ChainId } from "./chain";
 export * from "./client";
+export type { PonderIndexingStatus } from "./deserialize/indexing-status";
