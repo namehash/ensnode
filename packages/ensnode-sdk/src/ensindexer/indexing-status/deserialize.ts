2202a9ee60b07c66a3f235c0b4a8b101f86765f7
Tomasz Kopacki <tomasz@kopacki.net>
2026-02-11 13:12:07 +0100

    Extract OmnichainIndexingStatus deserialize-layer from generic deserialize.ts file

--- packages/ensnode-sdk/src/ensindexer/indexing-status/deserialize.ts
+++ packages/ensnode-sdk/src/ensindexer/indexing-status/deserialize.ts
@@ -1,36 +1,14 @@
 import { prettifyError } from "zod/v4";
 
-import type { OmnichainIndexingStatusSnapshot } from "./omnichain-indexing-status-snapshot";
-import type { SerializedOmnichainIndexingStatusSnapshot } from "./serialize/omnichain-indexing-status-snapshot";
 import type {
   SerializedCrossChainIndexingStatusSnapshot,
   SerializedRealtimeIndexingStatusProjection,
 } from "./serialized-types";
 import type { CrossChainIndexingStatusSnapshot, RealtimeIndexingStatusProjection } from "./types";
-import { makeOmnichainIndexingStatusSnapshotSchema } from "./zod-schema/omnichain-indexing-status-snapshot";
 import {
   makeCrossChainIndexingStatusSnapshotSchema,
   makeRealtimeIndexingStatusProjectionSchema,
 } from "./zod-schemas";
-
-/**
- * Deserialize an {@link OmnichainIndexingStatusSnapshot} object.
- */
-export function deserializeOmnichainIndexingStatusSnapshot(
-  maybeSnapshot: SerializedOmnichainIndexingStatusSnapshot,
-  valueLabel?: string,
-): OmnichainIndexingStatusSnapshot {
-  const schema = makeOmnichainIndexingStatusSnapshotSchema(valueLabel);
-  const parsed = schema.safeParse(maybeSnapshot);
-
-  if (parsed.error) {
-    throw new Error(
-      `Cannot deserialize into OmnichainIndexingStatusSnapshot:\n${prettifyError(parsed.error)}\n`,
-    );
-  }
-
-  return parsed.data;
-}
 
 /**
  * Deserialize an {@link CrossChainIndexingStatusSnapshot} object.
