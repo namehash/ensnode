63f28eaaf2b84d67aff3dbe4636dd75d6cf81231
Tomasz Kopacki <tomasz@kopacki.net>
2026-02-11 10:01:01 +0100

    Extract OmnichainIndexingStatus business-layer from generic helpers.ts file

--- packages/ensnode-sdk/src/ensindexer/indexing-status/omnichain-indexing-status-snapshot.ts
+++ packages/ensnode-sdk/src/ensindexer/indexing-status/omnichain-indexing-status-snapshot.ts
@@ -196,3 +196,146 @@ export type OmnichainIndexingStatusSnapshot =
   | OmnichainIndexingStatusSnapshotBackfill
   | OmnichainIndexingStatusSnapshotCompleted
   | OmnichainIndexingStatusSnapshotFollowing;
+
+/**
+ * Check if Chain Indexing Status Snapshots fit the 'unstarted' overall status
+ * snapshot requirements:
+ * - All chains are guaranteed to have a status of "queued".
+ *
+ * Note: This function narrows the {@link ChainIndexingStatusSnapshot} type to
+ * {@link ChainIndexingStatusSnapshotQueued}.
+ */
+export function checkChainIndexingStatusSnapshotsForOmnichainStatusSnapshotUnstarted(
+  chains: ChainIndexingStatusSnapshot[],
+): chains is ChainIndexingStatusSnapshotQueued[] {
+  return chains.every((chain) => chain.chainStatus === ChainIndexingStatusIds.Queued);
+}
+
+/**
+ * Check if Chain Indexing Status Snapshots fit the 'backfill' overall status
+ * snapshot requirements:
+ * - At least one chain is guaranteed to be in the "backfill" status.
+ * - Each chain is guaranteed to have a status of either "queued",
+ *   "backfill" or "completed".
+ *
+ * Note: This function narrows the {@link ChainIndexingStatusSnapshot} type to
+ * {@link ChainIndexingStatusSnapshotForOmnichainIndexingStatusSnapshotBackfill}.
+ */
+export function checkChainIndexingStatusSnapshotsForOmnichainStatusSnapshotBackfill(
+  chains: ChainIndexingStatusSnapshot[],
+): chains is ChainIndexingStatusSnapshotForOmnichainIndexingStatusSnapshotBackfill[] {
+  const atLeastOneChainInTargetStatus = chains.some(
+    (chain) => chain.chainStatus === ChainIndexingStatusIds.Backfill,
+  );
+  const otherChainsHaveValidStatuses = chains.every(
+    (chain) =>
+      chain.chainStatus === ChainIndexingStatusIds.Queued ||
+      chain.chainStatus === ChainIndexingStatusIds.Backfill ||
+      chain.chainStatus === ChainIndexingStatusIds.Completed,
+  );
+
+  return atLeastOneChainInTargetStatus && otherChainsHaveValidStatuses;
+}
+
+/**
+ * Checks if Chain Indexing Status Snapshots fit the 'completed' overall status
+ * snapshot requirements:
+ * - All chains are guaranteed to have a status of "completed".
+ *
+ * Note: This function narrows the {@link ChainIndexingStatusSnapshot} type to
+ * {@link ChainIndexingStatusSnapshotCompleted}.
+ */
+export function checkChainIndexingStatusSnapshotsForOmnichainStatusSnapshotCompleted(
+  chains: ChainIndexingStatusSnapshot[],
+): chains is ChainIndexingStatusSnapshotCompleted[] {
+  const allChainsHaveValidStatuses = chains.every(
+    (chain) => chain.chainStatus === ChainIndexingStatusIds.Completed,
+  );
+
+  return allChainsHaveValidStatuses;
+}
+
+/**
+ * Checks Chain Indexing Status Snapshots fit the 'following' overall status
+ * snapshot requirements:
+ * - At least one chain is guaranteed to be in the "following" status.
+ * - Any other chain can have any status.
+ */
+export function checkChainIndexingStatusSnapshotsForOmnichainStatusSnapshotFollowing(
+  chains: ChainIndexingStatusSnapshot[],
+): boolean {
+  const allChainsHaveValidStatuses = chains.some(
+    (chain) => chain.chainStatus === ChainIndexingStatusIds.Following,
+  );
+
+  return allChainsHaveValidStatuses;
+}
+
+/**
+ * Get {@link OmnichainIndexingStatusId} based on indexed chains' statuses.
+ *
+ * This function decides what is the `OmnichainIndexingStatusId` is,
+ * based on provided chain indexing statuses.
+ *
+ * @throws an error if unable to determine overall indexing status
+ */
+export function getOmnichainIndexingStatus(
+  chains: ChainIndexingStatusSnapshot[],
+): OmnichainIndexingStatusId {
+  if (checkChainIndexingStatusSnapshotsForOmnichainStatusSnapshotFollowing(chains)) {
+    return OmnichainIndexingStatusIds.Following;
+  }
+
+  if (checkChainIndexingStatusSnapshotsForOmnichainStatusSnapshotBackfill(chains)) {
+    return OmnichainIndexingStatusIds.Backfill;
+  }
+
+  if (checkChainIndexingStatusSnapshotsForOmnichainStatusSnapshotUnstarted(chains)) {
+    return OmnichainIndexingStatusIds.Unstarted;
+  }
+
+  if (checkChainIndexingStatusSnapshotsForOmnichainStatusSnapshotCompleted(chains)) {
+    return OmnichainIndexingStatusIds.Completed;
+  }
+
+  // if none of the chain statuses matched, throw an error
+  throw new Error(`Unable to determine omnichain indexing status for provided chains.`);
+}
+
+/**
+ * Get Omnichain Indexing Cursor
+ *
+ * The cursor tracks the "highest" latest indexed block timestamp across
+ * all indexed chains. If all chains are queued, the cursor tracks the moment
+ * just before the earliest start block timestamp across those chains.
+ *
+ * @throws an error if no chains are provided
+ */
+export function getOmnichainIndexingCursor(chains: ChainIndexingStatusSnapshot[]): UnixTimestamp {
+  if (chains.length === 0) {
+    throw new Error(`Unable to determine omnichain indexing cursor when no chains were provided.`);
+  }
+
+  // for omnichain indexing status snapshot 'unstarted', the cursor tracks
+  // the moment just before the indexing would start from.
+  if (getOmnichainIndexingStatus(chains) === OmnichainIndexingStatusIds.Unstarted) {
+    const earliestStartBlockTimestamps = chains.map((chain) => chain.config.startBlock.timestamp);
+
+    return Math.min(...earliestStartBlockTimestamps) - 1;
+  }
+
+  // otherwise, the cursor tracks the "highest" latest indexed block timestamp
+  // across all indexed chains
+  const latestIndexedBlockTimestamps = chains
+    .filter((chain) => chain.chainStatus !== ChainIndexingStatusIds.Queued)
+    .map((chain) => chain.latestIndexedBlock.timestamp);
+
+  // Invariant: there's at least one element in `latestIndexedBlockTimestamps` array
+  // This is theoretically impossible based on the 2 checks above,
+  // but the invariant is explicitly added here as a formality.
+  if (latestIndexedBlockTimestamps.length < 1) {
+    throw new Error("latestIndexedBlockTimestamps array must include at least one element");
+  }
+
+  return Math.max(...latestIndexedBlockTimestamps);
+}
