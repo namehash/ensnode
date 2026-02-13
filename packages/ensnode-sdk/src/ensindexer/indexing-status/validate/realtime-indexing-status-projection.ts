7988c80649c147bb3d493ee5329f6adfb4f90514
Tomasz Kopacki <tomasz@kopacki.net>
2026-02-13 12:02:50 +0100

    Created `RealtimeIndexingStatusProjection` validation-layer
    Validation-layer uses just business-level schema and does not rely on serialization-layer schema at all.

---
+++ packages/ensnode-sdk/src/ensindexer/indexing-status/validate/realtime-indexing-status-projection.ts
@@ -0,0 +1,24 @@
+import { prettifyError } from "zod/v4";
+
+import type { Unvalidated } from "../../../shared/types";
+import type { RealtimeIndexingStatusProjection } from "../realtime-indexing-status-projection";
+import { makeRealtimeIndexingStatusProjectionSchema } from "../zod-schema/realtime-indexing-status-projection";
+
+/**
+ * Validates a maybe {@link RealtimeIndexingStatusProjection} object.
+ *
+ * @throws Error if the provided object is not a valid {@link RealtimeIndexingStatusProjection}.
+ */
+export function validateRealtimeIndexingStatusProjection(
+  unvalidatedProjection: Unvalidated<RealtimeIndexingStatusProjection>,
+  valueLabel?: string,
+): RealtimeIndexingStatusProjection {
+  const schema = makeRealtimeIndexingStatusProjectionSchema(valueLabel);
+  const parsed = schema.safeParse(unvalidatedProjection);
+
+  if (parsed.error) {
+    throw new Error(`Invalid RealtimeIndexingStatusProjection:\n${prettifyError(parsed.error)}\n`);
+  }
+
+  return parsed.data;
+}
