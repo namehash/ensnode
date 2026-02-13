be2984ec12bbfae6f6493a08ebd31bee5f991695
Tomasz Kopacki <tomasz@kopacki.net>
2026-02-13 12:04:38 +0100

    Split zod-schemas for `IndexingStatusResponse`
    Create independent schemas for each layer, business and serialization

--- packages/ensnode-sdk/src/api/indexing-status/zod-schemas.ts
+++ packages/ensnode-sdk/src/api/indexing-status/zod-schemas.ts
@@ -1,12 +1,19 @@
 import { z } from "zod/v4";
 
-import { makeRealtimeIndexingStatusProjectionSchema } from "../../ensindexer/indexing-status/zod-schema/realtime-indexing-status-projection";
+import {
+  makeRealtimeIndexingStatusProjectionSchema,
+  makeSerializedRealtimeIndexingStatusProjectionSchema,
+} from "../../ensindexer/indexing-status/zod-schema/realtime-indexing-status-projection";
 import {
   type IndexingStatusResponse,
   IndexingStatusResponseCodes,
   type IndexingStatusResponseError,
   type IndexingStatusResponseOk,
 } from "./response";
+import {
+  SerializedIndexingStatusResponse,
+  SerializedIndexingStatusResponseOk,
+} from "./serialized-response";
 
 /**
  * Schema for {@link IndexingStatusResponseOk}
@@ -35,5 +42,27 @@ export const makeIndexingStatusResponseErrorSchema = (
 export const makeIndexingStatusResponseSchema = (valueLabel: string = "Indexing Status Response") =>
   z.discriminatedUnion("responseCode", [
     makeIndexingStatusResponseOkSchema(valueLabel),
+    makeIndexingStatusResponseErrorSchema(valueLabel),
+  ]);
+
+/**
+ * Schema for {@link SerializedIndexingStatusResponseOk}
+ **/
+export const makeSerializedIndexingStatusResponseOkSchema = (
+  valueLabel: string = "Serialized Indexing Status Response OK",
+) =>
+  z.strictObject({
+    responseCode: z.literal(IndexingStatusResponseCodes.Ok),
+    realtimeProjection: makeSerializedRealtimeIndexingStatusProjectionSchema(valueLabel),
+  });
+
+/**
+ * Schema for {@link SerializedIndexingStatusResponse}
+ **/
+export const makeSerializedIndexingStatusResponseSchema = (
+  valueLabel: string = "Serialized Indexing Status Response",
+) =>
+  z.discriminatedUnion("responseCode", [
+    makeSerializedIndexingStatusResponseOkSchema(valueLabel),
     makeIndexingStatusResponseErrorSchema(valueLabel),
   ]);
