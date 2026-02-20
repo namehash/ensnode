f3414c1754484522f190f1ea89125170997b1443
Tomasz Kopacki <tomasz@kopacki.net>
2026-02-20 13:05:59 +0100

    Make `getLocalPonderClient` sync function
    Now, that async data fetching has been delegated to SWR caches, creating a singleton instnace for `LocalPonderClient` is way simpler, _and synchronous_!

--- apps/ensindexer/src/lib/ponder-api-client.ts
+++ apps/ensindexer/src/lib/ponder-api-client.ts
@@ -1,62 +1,31 @@
 import config from "@/config";
 
-import pRetry from "p-retry";
-
+import { chainsIndexingMetadataImmutableCache } from "@/ponder/api/lib/cache/chains-indexing-metadata-immutable.cache";
+import { ponderClientCache } from "@/ponder/api/lib/cache/ponder-client.cache";
 import { LocalPonderClient } from "@/ponder/api/lib/local-ponder-client";
 
-let localPonderClientPromise: Promise<LocalPonderClient>;
+let localPonderClient: LocalPonderClient | undefined = undefined;
 
 /**
  * Get the singleton LocalPonderClient instance for the ENSIndexer app.
  *
- * This function initializes the LocalPonderClient on the first call by
- * connecting to the local Ponder app and fetching the necessary data to
- * build the client's state. The initialized client is cached and returned
- * on subsequent calls.
+ * This function relies on SWR caches with proactive revalidation to load
+ * necessary data for the client state, allowing the client to be initialized
+ * in a non-blocking way. The function will return the cached client instance.
  *
  * @returns The singleton LocalPonderClient instance.
- * @throws Error if the client fails to initialize after
- *         the specified number of retries.
  */
-export async function getLocalPonderClient(): Promise<LocalPonderClient> {
-  // Return the cached client instance if it has already been initialized.
-  if (localPonderClientPromise) {
-    return localPonderClientPromise;
+export function getLocalPonderClient(): LocalPonderClient {
+  // Initialize the singleton LocalPonderClient instance if it hasn't been
+  // initialized yet.
+  if (localPonderClient === undefined) {
+    localPonderClient = new LocalPonderClient(
+      config.ensIndexerUrl,
+      config.indexedChainIds,
+      chainsIndexingMetadataImmutableCache,
+      ponderClientCache,
+    );
   }
 
-  // Initialize the LocalPonderClient in a non-blocking way.
-  // Apply retries in case of failure, for example, if the Ponder app is
-  // not yet ready to accept connections.
-  /**
-   * Initialize the LocalPonderClient by connecting to the local Ponder app and
-   * fetching necessary data to build the client's state. This operation is
-   * retried up to 3 times in case of failure, with a warning logged on each
-   * failed attempt.
-   *
-   * @returns The initialized LocalPonderClient instance.
-   * @throws Error if the client fails to initialize after the specified number of retries.
-   */
-  localPonderClientPromise = pRetry(
-    () => LocalPonderClient.init(config.ensIndexerUrl, config.indexedChainIds),
-    {
-      retries: 3,
-
-      onFailedAttempt: ({ error, attemptNumber, retriesLeft }) => {
-        console.warn(
-          `Initializing local Ponder client attempt ${attemptNumber} failed (${error.message}). ${retriesLeft} retries left.`,
-        );
-      },
-    },
-  ).catch((error) => {
-    console.error(
-      `Failed to initialize LocalPonderClient after multiple attempts: ${error.message}`,
-    );
-
-    // Signal termination of the process with a non-zero exit code to indicate failure.
-    process.exitCode = 1;
-
-    throw error;
-  });
-
-  return localPonderClientPromise;
+  return localPonderClient;
 }
