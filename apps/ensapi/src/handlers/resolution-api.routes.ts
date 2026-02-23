0d515e2bba731897343e58972691809727ddb6f9
Jamie Barton <jamie@notrab.dev>
2026-02-23 07:15:35 +0000

    feat: add routes api

---
+++ apps/ensapi/src/handlers/resolution-api.routes.ts
@@ -0,0 +1,83 @@
+import { createRoute } from "@hono/zod-openapi";
+import { z } from "zod/v4";
+
+import { params } from "@/lib/handlers/params.schema";
+
+export const basePath = "/resolve";
+
+export const resolveRecordsRoute = createRoute({
+  method: "get",
+  path: "/records/{name}",
+  tags: ["Resolution"],
+  summary: "Resolve ENS Records",
+  description: "Resolves ENS records for a given name",
+  request: {
+    params: z.object({
+      name: params.name,
+    }),
+    query: z
+      .object({
+        ...params.selectionParams.shape,
+        trace: params.trace,
+        accelerate: params.accelerate,
+      })
+      .transform((value) => {
+        const { trace, accelerate, ...selectionParams } = value;
+        const selection = params.selection.parse(selectionParams);
+        return { selection, trace, accelerate };
+      }),
+  },
+  responses: {
+    200: {
+      description: "Successfully resolved records",
+    },
+  },
+});
+
+export const resolvePrimaryNameRoute = createRoute({
+  method: "get",
+  path: "/primary-name/{address}/{chainId}",
+  tags: ["Resolution"],
+  summary: "Resolve Primary Name",
+  description: "Resolves a primary name for a given `address` and `chainId`",
+  request: {
+    params: z.object({
+      address: params.address,
+      chainId: params.defaultableChainId,
+    }),
+    query: z.object({
+      trace: params.trace,
+      accelerate: params.accelerate,
+    }),
+  },
+  responses: {
+    200: {
+      description: "Successfully resolved name",
+    },
+  },
+});
+
+export const resolvePrimaryNamesRoute = createRoute({
+  method: "get",
+  path: "/primary-names/{address}",
+  tags: ["Resolution"],
+  summary: "Resolve Primary Names",
+  description: "Resolves all primary names for a given address across multiple chains",
+  request: {
+    params: z.object({
+      address: params.address,
+    }),
+    query: z.object({
+      chainIds: params.chainIdsWithoutDefaultChainId,
+      trace: params.trace,
+      accelerate: params.accelerate,
+    }),
+  },
+  responses: {
+    200: {
+      description: "Successfully resolved records",
+    },
+  },
+});
+
+export const routes = [resolveRecordsRoute, resolvePrimaryNameRoute, resolvePrimaryNamesRoute];
