1d4594074deddeb95d25d727913391c941bddf33
Tomasz Kopacki <tomasz@kopacki.net>
2026-02-24 10:50:55 +0100

    Update `useIndexingStatusWithSwr` hook to also cache `config: EnsApiPublicConfig` value.

--- apps/ensadmin/src/components/indexing-status/use-indexing-status-with-swr.ts
+++ apps/ensadmin/src/components/indexing-status/use-indexing-status-with-swr.ts
@@ -7,40 +7,49 @@ import { useCallback, useMemo } from "react";
 import {
   createIndexingStatusQueryOptions,
   QueryParameter,
-  useENSNodeSDKConfig,
+  useEnsApiProviderOptions,
   type useIndexingStatus,
   useSwrQuery,
-  WithSDKConfigParameter,
+  WithEnsApiProviderOptions,
 } from "@ensnode/ensnode-react";
 import {
   CrossChainIndexingStatusSnapshotOmnichain,
   createRealtimeIndexingStatusProjection,
   Duration,
-  type IndexingStatusRequest,
-  IndexingStatusResponseCodes,
-  IndexingStatusResponseOk,
+  EnsApiIndexingStatusRequest,
+  EnsApiIndexingStatusResponseCodes,
+  EnsApiIndexingStatusResponseOk,
+  EnsApiPublicConfig,
 } from "@ensnode/ensnode-sdk";
 
 const DEFAULT_REFETCH_INTERVAL = secondsToMilliseconds(10);
 
 const REALTIME_PROJECTION_REFRESH_RATE: Duration = 1;
 
+interface CachableIndexingStatus {
+  crossChainIndexingStatusSnapshot: CrossChainIndexingStatusSnapshotOmnichain;
+  config: EnsApiPublicConfig;
+}
+
 interface UseIndexingStatusParameters
-  extends IndexingStatusRequest,
-    QueryParameter<CrossChainIndexingStatusSnapshotOmnichain> {}
+  extends EnsApiIndexingStatusRequest,
+    QueryParameter<CachableIndexingStatus> {}
 
 /**
  * A proxy hook for {@link useIndexingStatus} which applies
  * stale-while-revalidate cache for successful responses.
  */
 export function useIndexingStatusWithSwr(
-  parameters: WithSDKConfigParameter & UseIndexingStatusParameters = {},
+  parameters: WithEnsApiProviderOptions & UseIndexingStatusParameters = {},
 ) {
-  const { config, query = {} } = parameters;
-  const _config = useENSNodeSDKConfig(config);
+  const { options, query = {} } = parameters;
+  const providerOptions = useEnsApiProviderOptions(options);
   const now = useNow({ timeToRefresh: REALTIME_PROJECTION_REFRESH_RATE });
 
-  const queryOptions = useMemo(() => createIndexingStatusQueryOptions(_config), [_config]);
+  const queryOptions = useMemo(
+    () => createIndexingStatusQueryOptions(providerOptions),
+    [providerOptions],
+  );
   const queryKey = useMemo(() => ["swr", ...queryOptions.queryKey], [queryOptions.queryKey]);
   const queryFn = useCallback(
     async () =>
@@ -48,17 +57,21 @@ export function useIndexingStatusWithSwr(
         // An indexing status response was successfully fetched,
         // but the response code contained within the response was not 'ok'.
         // Therefore, throw an error to avoid caching this response.
-        if (response.responseCode !== IndexingStatusResponseCodes.Ok) {
+        if (response.responseCode !== EnsApiIndexingStatusResponseCodes.Ok) {
           throw new Error(
             "Received Indexing Status response with responseCode other than 'ok' which will not be cached.",
           );
         }
 
-        // The indexing status snapshot has been fetched and successfully validated for caching.
+        // The indexing status snapshot, including ENSApi public config,
+        // has been fetched and successfully validated for caching.
         // Therefore, return it so that query cache for `queryOptions.queryKey` will:
         // - Replace the currently cached value (if any) with this new value.
         // - Return this non-null value.
-        return response.realtimeProjection.snapshot;
+        return {
+          crossChainIndexingStatusSnapshot: response.realtimeProjection.snapshot,
+          config: response.config,
+        } satisfies CachableIndexingStatus;
       }),
     [queryOptions.queryFn],
   );
@@ -66,16 +79,20 @@ export function useIndexingStatusWithSwr(
   // Call select function to `createRealtimeIndexingStatusProjection` each time
   // `now` is updated.
   const select = useCallback(
-    (cachedSnapshot: CrossChainIndexingStatusSnapshotOmnichain): IndexingStatusResponseOk => {
-      const realtimeProjection = createRealtimeIndexingStatusProjection(cachedSnapshot, now);
+    (cachedResult: CachableIndexingStatus): EnsApiIndexingStatusResponseOk => {
+      const realtimeProjection = createRealtimeIndexingStatusProjection(
+        cachedResult.crossChainIndexingStatusSnapshot,
+        now,
+      );
 
-      // Maintain the original response shape of `IndexingStatusResponse`
+      // Maintain the original response shape of `EnsApiIndexingStatusResponseOk`
       // for the consumers. Creating a new projection from the cached snapshot
       // each time `now` is updated should be implementation detail.
       return {
-        responseCode: IndexingStatusResponseCodes.Ok,
+        responseCode: EnsApiIndexingStatusResponseCodes.Ok,
         realtimeProjection,
-      } satisfies IndexingStatusResponseOk;
+        config: cachedResult.config,
+      } satisfies EnsApiIndexingStatusResponseOk;
     },
     [now],
   );
