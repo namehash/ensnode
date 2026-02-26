037d942373f090b954e94cac04b59081f2e91e1a
Jamie Barton <jamie@notrab.dev>
2026-02-26 07:02:10 +0000

    refactor: use zod-openapi for ensanalytics-api

--- apps/ensapi/src/stub-routes.ts
+++ apps/ensapi/src/stub-routes.ts
@@ -1,6 +1,7 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 
 import * as amIRealtimeRoutes from "./handlers/amirealtime-api.routes";
+import * as ensanalyticsRoutes from "./handlers/ensanalytics-api.routes";
 import * as ensnodeRoutes from "./handlers/ensnode-api.routes";
 import * as resolutionRoutes from "./handlers/resolution-api.routes";
 
@@ -12,7 +13,7 @@ import * as resolutionRoutes from "./handlers/resolution-api.routes";
 export function createStubRoutesForSpec() {
   const app = new OpenAPIHono();
 
-  const routeGroups = [amIRealtimeRoutes, ensnodeRoutes, resolutionRoutes];
+  const routeGroups = [amIRealtimeRoutes, ensanalyticsRoutes, ensnodeRoutes, resolutionRoutes];
 
   for (const group of routeGroups) {
     for (const route of group.routes) {
