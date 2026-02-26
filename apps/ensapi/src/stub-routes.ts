ddaa68429dac63e9d014e308f120fa789cada232
Jamie Barton <jamie@notrab.dev>
2026-02-26 08:32:03 +0000

    reorder routes

--- apps/ensapi/src/stub-routes.ts
+++ apps/ensapi/src/stub-routes.ts
@@ -4,8 +4,8 @@ import * as amIRealtimeRoutes from "./handlers/amirealtime-api.routes";
 import * as ensanalyticsRoutes from "./handlers/ensanalytics-api.routes";
 import * as ensanalyticsV1Routes from "./handlers/ensanalytics-api-v1.routes";
 import * as ensnodeRoutes from "./handlers/ensnode-api.routes";
-import * as registrarActionsRoutes from "./handlers/registrar-actions-api.routes";
 import * as nameTokensRoutes from "./handlers/name-tokens-api.routes";
+import * as registrarActionsRoutes from "./handlers/registrar-actions-api.routes";
 import * as resolutionRoutes from "./handlers/resolution-api.routes";
 
 /**
