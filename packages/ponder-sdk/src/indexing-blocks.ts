4c186a81c102ba640683b6ac234ae7b0473233a6
Tomasz Kopacki <tomasz@kopacki.net>
2026-02-28 20:24:11 +0100

    Replace `ChainIndexingBlocks` with `ChainIndexingConfig`

--- packages/ponder-sdk/src/indexing-blocks.ts
+++
@@ -1,7 +0,0 @@
-import type { BlockNumber } from "./blocks";
-
-export interface ChainIndexingBlocks {
-  startBlock: BlockNumber;
-  endBlock?: BlockNumber | null;
-  backfillEndBlock: BlockNumber;
-}
