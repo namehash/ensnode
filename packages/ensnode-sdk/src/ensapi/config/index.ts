b3ac81b541b218a6fc68305c045b0bfb07037b62
Tomasz Kopacki <tomasz@kopacki.net>
2026-03-04 14:49:43 +0100

    Create validation helper and schemas for ENSApi public config

--- packages/ensnode-sdk/src/ensapi/config/index.ts
+++ packages/ensnode-sdk/src/ensapi/config/index.ts
@@ -2,4 +2,5 @@ export * from "./deserialize";
 export * from "./serialize";
 export * from "./serialized-types";
 export * from "./types";
+export * from "./validate/ensapi-public-config";
 export * from "./zod-schemas";
