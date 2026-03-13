f35edb0348fcd6cb4ad5549deaea56262603a997
Tomasz Kopacki <tomasz@kopacki.net>
2026-03-13 18:19:29 +0100

    Fix exports for ENSDb Schema definitions

--- apps/ensindexer/ponder/ponder.schema.ts
+++ apps/ensindexer/ponder/ponder.schema.ts
@@ -1,2 +1,2 @@
-// export the shared ponder schema
-export * from "@ensnode/ensnode-schema";
+// export the ENSIndexer schema for Ponder to use when indexing data
+export * from "@ensnode/ensnode-schema/ensindexer";
--- packages/ensnode-schema/src/index.ts
+++ packages/ensnode-schema/src/index.ts
@@ -1,3 +1,5 @@
 export * from "./ensindexer-db";
 export * from "./ensnode-db";
 export * from "./lib/drizzle";
+export * from "./ensnode-schema";
+export * from "./ensindexer-schema";
