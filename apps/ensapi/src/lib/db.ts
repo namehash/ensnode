12359f82ad96fc86968574de15e364f336ac701a
Tomasz Kopacki <tomasz@kopacki.net>
2026-03-13 18:19:01 +0100

    Integrate `EnsIndexerDbReader` into ENSApi`

--- apps/ensapi/src/lib/db.ts
+++ apps/ensapi/src/lib/db.ts
@@ -1,14 +1,22 @@
 import config from "@/config";
 
-import * as ensIndexerSchema from "@ensnode/ensnode-schema/ensindexer";
+import { EnsIndexerDbReader } from "@ensnode/ensnode-schema";
 
-import { makeReadOnlyDrizzle } from "@/lib/handlers/drizzle";
+// TODO: pending rename `config.databaseUrl` to `config.ensDbUrl`
+// Will be executed once https://github.com/namehash/ensnode/issues/1763 is resolved.
+const ensDbUrl = config.databaseUrl;
+// TODO: pending rename `config.databaseSchemaName` to `config.ensIndexerSchemaName`
+// Will be executed once https://github.com/namehash/ensnode/issues/1762 is resolved.
+const ensIndexerSchemaName = config.databaseSchemaName;
+
+const ensIndexerDbReader = new EnsIndexerDbReader(ensDbUrl, ensIndexerSchemaName);
 
 /**
- * Read-only Drizzle instance for ENSDb queries to ENSIndexer Schema
+ * Read-only Drizzle instance for queries to ENSIndexer Schema in ENSDb.
  */
-export const db = makeReadOnlyDrizzle({
-  databaseUrl: config.databaseUrl,
-  databaseSchema: config.databaseSchemaName,
-  schema: ensIndexerSchema,
-});
+export const ensIndexerDbReadonly = ensIndexerDbReader.db;
+
+/**
+ * Read-only Drizzle instance for queries to ENSIndexer Schema in ENSDb.
+ */
+export const db = ensIndexerDbReadonly;
