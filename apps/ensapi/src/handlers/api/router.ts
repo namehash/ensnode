dc018568753897b769edc15b8c61bcf59b833c95
sevenzing <lymarenko.lev@gmail.com>
2026-03-19 21:03:59 +0300

    rename ensenodeRoute to statusRoute

--- apps/ensapi/src/handlers/api/router.ts
+++ apps/ensapi/src/handlers/api/router.ts
@@ -3,13 +3,13 @@ import { createApp } from "@/lib/hono-factory";
 import nameTokensApi from "./explore/name-tokens-api";
 import registrarActionsApi from "./explore/registrar-actions-api";
 import ensnodeGraphQLApi from "./graphql/ensnode-graphql-api";
-import metaApi from "./meta/ensnode-api";
+import statusApi from "./meta/status-api";
 import realtimeApi from "./meta/realtime-api";
 import resolutionApi from "./resolution/resolution-api";
 
 const app = createApp();
 
-app.route("/", metaApi);
+app.route("/", statusApi);
 app.route("/realtime", realtimeApi);
 app.route("/resolve", resolutionApi);
 app.route("/name-tokens", nameTokensApi);
--- apps/ensapi/src/openapi-document.ts
+++ apps/ensapi/src/openapi-document.ts
@@ -4,7 +4,7 @@ import { openapiMeta } from "@/openapi-meta";
 
 import * as nameTokensRoutes from "./handlers/api/explore/name-tokens-api.routes";
 import * as registrarActionsRoutes from "./handlers/api/explore/registrar-actions-api.routes";
-import * as ensnodeRoutes from "./handlers/api/meta/ensnode-api.routes";
+import * as statusRoutes from "./handlers/api/meta/status-api.routes";
 import * as realtimeRoutes from "./handlers/api/meta/realtime-api.routes";
 import * as resolutionRoutes from "./handlers/api/resolution/resolution-api.routes";
 import * as ensanalyticsRoutes from "./handlers/ensanalytics/ensanalytics-api.routes";
@@ -12,7 +12,7 @@ import * as ensanalyticsV1Routes from "./handlers/ensanalytics/ensanalytics-api-
 
 const routeGroups = [
   realtimeRoutes,
-  ensnodeRoutes,
+  statusRoutes,
   ensanalyticsV1Routes,
   ensanalyticsRoutes,
   nameTokensRoutes,
