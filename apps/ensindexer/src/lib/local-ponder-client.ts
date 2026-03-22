8b5f1d7310a3fd7666060b85387a97a0845d3a5e
Tomasz Kopacki <tomasz@kopacki.net>
2026-03-22 08:34:44 +0100

    Make `localPonderClient` for ENSIndexer to accept the Ponder app context

--- apps/ensindexer/src/lib/local-ponder-client.ts
+++ apps/ensindexer/src/lib/local-ponder-client.ts
@@ -3,16 +3,18 @@ import config from "@/config";
 import { publicClients } from "ponder:api";
 
 import { buildIndexedBlockranges } from "@ensnode/ensnode-sdk";
-import { LocalPonderClient } from "@ensnode/ponder-sdk";
+import { deserializePonderAppContext, LocalPonderClient } from "@ensnode/ponder-sdk";
 
 import { getPluginsAllDatasourceNames } from "@/lib/plugin-helpers";
 
 const pluginsAllDatasourceNames = getPluginsAllDatasourceNames(config.plugins);
 const indexedBlockranges = buildIndexedBlockranges(config.namespace, pluginsAllDatasourceNames);
+const ponderAppContext = deserializePonderAppContext(globalThis.PONDER_COMMON);
 
 export const localPonderClient = new LocalPonderClient(
   config.ensIndexerUrl,
   config.indexedChainIds,
   indexedBlockranges,
   publicClients,
+  ponderAppContext,
 );
--- apps/ensindexer/types/env.d.ts
+++ apps/ensindexer/types/env.d.ts
@@ -4,4 +4,16 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv extends ENSIndexerEnvironment {}
   }
+
+  /**
+   * Global variable injected by Ponder at runtime,
+   * containing internal context of the local Ponder app.
+   *
+   * @see https://github.com/ponder-sh/ponder/blob/6fcc15d4234e43862cb6e21c05f3c57f4c2f7464/packages/core/src/internal/common.ts#L7-L15
+   */
+  var PONDER_COMMON: {
+    options: {
+      command: string;
+    };
+  };
 }
