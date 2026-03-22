48c369e40b851a1d50c9469fb0719aed5b8b02d8
Tomasz Kopacki <tomasz@kopacki.net>
2026-03-22 08:53:51 +0100

    ENSDb Writer Worker conditional validation
    Skip ENSIndexer Pulic Config validation in dev mode.

--- apps/ensindexer/src/lib/ensdb-writer-worker/ensdb-writer-worker.ts
+++ apps/ensindexer/src/lib/ensdb-writer-worker/ensdb-writer-worker.ts
@@ -11,6 +11,7 @@ import {
   type OmnichainIndexingStatusSnapshot,
   validateEnsIndexerPublicConfigCompatibility,
 } from "@ensnode/ensnode-sdk";
+import type { LocalPonderClient } from "@ensnode/ponder-sdk";
 
 import type { IndexingStatusBuilder } from "@/lib/indexing-status-builder/indexing-status-builder";
 import type { PublicConfigBuilder } from "@/lib/public-config-builder/public-config-builder";
@@ -51,18 +52,28 @@ export class EnsDbWriterWorker {
   private publicConfigBuilder: PublicConfigBuilder;
 
   /**
+   * Local Ponder Client instance
+   *
+   * Used to get local Ponder app command.
+   */
+  private localPonderClient: LocalPonderClient;
+
+  /**
    * @param ensDbClient ENSDb Writer instance used by the worker to interact with ENSDb.
    * @param publicConfigBuilder ENSIndexer Public Config Builder instance used by the worker to read ENSIndexer Public Config.
    * @param indexingStatusBuilder Indexing Status Builder instance used by the worker to read ENSIndexer Indexing Status.
+   * @param localPonderClient Local Ponder Client instance, used to get local Ponder app command.
    */
   constructor(
     ensDbClient: EnsDbWriter,
     publicConfigBuilder: PublicConfigBuilder,
     indexingStatusBuilder: IndexingStatusBuilder,
+    localPonderClient: LocalPonderClient,
   ) {
     this.ensDbClient = ensDbClient;
     this.publicConfigBuilder = publicConfigBuilder;
     this.indexingStatusBuilder = indexingStatusBuilder;
+    this.localPonderClient = localPonderClient;
   }
 
   /**
@@ -181,8 +192,12 @@ export class EnsDbWriterWorker {
     }
 
     // Validate in-memory config object compatibility with the stored one,
-    // if the stored one is available
-    if (storedConfig) {
+    // if the stored one is available.
+    // The validation is skipped if the local Ponder app is running in dev mode.
+    // This is to improve the development experience during ENSIndexer
+    // development, by allowing to override the stored config in ENSDb with
+    // the current in-memory config, without having to keep them compatible.
+    if (storedConfig && !this.localPonderClient.isInDevMode) {
       try {
         validateEnsIndexerPublicConfigCompatibility(storedConfig, inMemoryConfig);
       } catch (error) {
