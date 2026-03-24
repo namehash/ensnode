a8b7f861fe19309f4d335b6d6795cfa43afa43ff
Tomasz Kopacki <tomasz@kopacki.net>
2026-03-24 17:00:58 +0100

    Reanme `ensDbConnectionString` to `ensDbUrl`

--- apps/ensapi/src/config/environment.ts
+++ apps/ensapi/src/config/environment.ts
@@ -1,5 +1,5 @@
 import type {
-  DatabaseEnvironment,
+  EnsApiDatabaseEnvironment,
   LogLevelEnvironment,
   PortEnvironment,
   ReferralProgramEditionsEnvironment,
@@ -8,20 +8,13 @@ import type {
 } from "@ensnode/ensnode-sdk/internal";
 
 /**
- * Environment variables for ENSApi configuration related to ENSDb.
- */
-interface EnsApiEnvironmentForEnsDb extends Omit<DatabaseEnvironment, "DATABASE_SCHEMA"> {
-  ENSINDEXER_SCHEMA_NAME?: string;
-}
-
-/**
  * Represents the raw, unvalidated environment variables for the ENSApi application.
  *
  * Keys correspond to the environment variable names, and all values are optional strings, reflecting
  * their state in `process.env`. This interface is intended to be the source type which then gets
  * mapped/parsed into a structured configuration object like `EnsApiConfig`.
  */
-export type EnsApiEnvironment = EnsApiEnvironmentForEnsDb &
+export type EnsApiEnvironment = EnsApiDatabaseEnvironment &
   RpcEnvironment &
   PortEnvironment &
   LogLevelEnvironment &
