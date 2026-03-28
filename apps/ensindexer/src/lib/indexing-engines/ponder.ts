5ed57deb2a21705be38e37c092fcaaf4f80f2779
Tomasz Kopacki <tomasz@kopacki.net>
2026-03-28 15:03:16 +0100

    Update `context` param data model
    Allow using `context.ensDb` alias for accessing the cached Drizzle client for ENSDb.

--- apps/ensindexer/src/lib/indexing-engines/ponder.ts
+++ apps/ensindexer/src/lib/indexing-engines/ponder.ts
@@ -1,17 +1,70 @@
-import { type EventNames, type IndexingFunctionArgs, ponder } from "ponder:registry";
+import {
+  type EventNames,
+  type Context as PonderIndexingContext,
+  type Event as PonderIndexingEvent,
+  ponder,
+} from "ponder:registry";
 
 /**
- * Adds an event listener for a specific event.
- *
- * This is a thin wrapper around `ponder.on` that allows executing additional logic
- * before or after the event handler if needed.
- *
+ * Context passed to event handlers registered with
+ * {@link addOnchainEventListener}.
+ */
+export interface IndexingEngineContext extends PonderIndexingContext<EventNames> {
+  ensDb: PonderIndexingContext<EventNames>["db"];
+}
+
+/**
+ * Event passed to event handlers registered with
+ * {@link addOnchainEventListener}.
+ */
+export type IndexingEngineEvent<EventName extends EventNames = EventNames> =
+  PonderIndexingEvent<EventName>;
+
+/**
+ * Args passed to event handlers registered with
+ * {@link addOnchainEventListener}.
+ */
+export interface IndexingEngineEventHandlerArgs<EventName extends EventNames = EventNames> {
+  context: IndexingEngineContext;
+  event: IndexingEngineEvent<EventName>;
+}
+
+/**
+ * Build the context passed to event handlers registered
+ * with {@link addOnchainEventListener} from the context provided by
+ * Ponder. This is where we can add any additional properties,
+ * or helper functions to the context that we want to be available in
+ * all of our event handlers.
+ */
+function buildIndexingEngineContext(
+  ponderContext: PonderIndexingContext<EventNames>,
+): IndexingEngineContext {
+  return {
+    ...ponderContext,
+    ensDb: ponderContext.db,
+  };
+}
+
+/**
+ * A thin wrapper around `ponder.on` that allows us to:
+ * - Provide custom context to event handlers.
+ * - Execute additional logic  before or after the event handler, if needed.
+ * 
+ * Note that this function is called on every event, so it should be
+ * efficient and avoid doing any heavy computations or database queries.
+ * 
  * For more details on `ponder.on`, see the Ponder indexing guide.
  * @see https://ponder.sh/docs/indexing/overview#register-an-indexing-function
+ 
  */
-export function addOnchainEventListener<const EventName extends EventNames>(
+export function addOnchainEventListener<EventName extends EventNames>(
   eventName: EventName,
-  eventHandler: (args: IndexingFunctionArgs<EventName>) => Promise<void> | void,
+  eventHandler: (args: IndexingEngineEventHandlerArgs<EventName>) => Promise<void> | void,
 ) {
-  return ponder.on(eventName, eventHandler);
+  return ponder.on(eventName, ({ context, event }) =>
+    eventHandler({
+      context: buildIndexingEngineContext(context),
+      event,
+    }),
+  );
 }
