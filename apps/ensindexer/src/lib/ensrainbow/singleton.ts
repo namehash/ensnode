f9e4e115498c040e1d7867dad1391a944790f17e
Tomasz Kopacki <tomasz@kopacki.net>
2026-03-30 08:49:52 +0200

    Simplify `ensrainbow/signleton.ts` file

--- apps/ensindexer/src/lib/ensrainbow/singleton.ts
+++ apps/ensindexer/src/lib/ensrainbow/singleton.ts
@@ -1,26 +1,19 @@
 import config from "@/config";
 
-import { type EnsRainbow, EnsRainbowApiClient } from "@ensnode/ensrainbow-sdk";
+import { EnsRainbowApiClient } from "@ensnode/ensrainbow-sdk";
 
-export function getENSRainbowApiClient(): EnsRainbow.ApiClient {
-  const ensRainbowApiClient = new EnsRainbowApiClient({
-    endpointUrl: config.ensRainbowUrl,
-    labelSet: config.labelSet,
-  });
+const { ensRainbowUrl, labelSet } = config;
 
-  if (
-    ensRainbowApiClient.getOptions().endpointUrl ===
-    EnsRainbowApiClient.defaultOptions().endpointUrl
-  ) {
-    console.warn(
-      `Using default public ENSRainbow server which may cause increased network latency. For production, use your own ENSRainbow server that runs on the same network as the ENSIndexer server.`,
-    );
-  }
-
-  return ensRainbowApiClient;
+if (ensRainbowUrl === EnsRainbowApiClient.defaultOptions().endpointUrl) {
+  console.warn(
+    `Using default public ENSRainbow server which may cause increased network latency. For production, use your own ENSRainbow server that runs on the same network as the ENSIndexer server.`,
+  );
 }
 
 /**
  * Singleton ENSRainbow API Client instance for ENSIndexer app.
  */
-export const ensRainbowClient = getENSRainbowApiClient();
+export const ensRainbowClient = new EnsRainbowApiClient({
+  endpointUrl: ensRainbowUrl,
+  labelSet,
+});
