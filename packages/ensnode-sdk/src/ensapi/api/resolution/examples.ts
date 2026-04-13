5aebdf16bc81485440b8c0ac95384af592158036
sevenzing <lymarenko.lev@gmail.com>
2026-04-13 20:55:18 +0400

    do not use reverseRecord in example

--- packages/ensnode-sdk/src/ensapi/api/resolution/examples.ts
+++ packages/ensnode-sdk/src/ensapi/api/resolution/examples.ts
@@ -3,7 +3,6 @@
  */
 export const resolveRecordsResponseExample = {
   records: {
-    name: "vitalik.eth",
     addresses: { "60": "0xd8da6bf26964af9d7eed9e03e53415d37aa96045" },
     texts: {
       description: "mi pinxe lo crino tcati",
