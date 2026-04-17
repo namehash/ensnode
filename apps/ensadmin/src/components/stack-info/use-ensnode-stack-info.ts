774a1055ccbd71dbc47ec13a8897b76d79396bd9
Tomasz Kopacki <tomasz@kopacki.net>
2026-04-17 18:37:50 +0200

    Replace `useEnsApiPublicConfig` hook with `useEnsNodeStackInfo` hook

---
+++ apps/ensadmin/src/components/stack-info/use-ensnode-stack-info.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { useEnsNodeProviderOptions } from "@ensnode/ensnode-react";
+
+import { useIndexingStatusWithSwr } from "@/components/indexing-status";
+
+/**
+ * Use the ENSNode Stack Info for the currently selected connection.
+ *
+ * This is a convenience hook that abstracts away the details of
+ * extracting the ENSNode Stack Info from the Indexing Status query.
+ */
+export function useEnsNodeStackInfo() {
+  const indexingStatusSwr = useIndexingStatusWithSwr();
+
+  return { ...indexingStatusSwr, data: indexingStatusSwr.data?.stackInfo };
+}
