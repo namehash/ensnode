955db50d80b5020778d6bc2f316d07569372f71d
Jamie Barton <jamie@notrab.dev>
2026-04-22 15:07:51 +0100

    apply code suggestions

--- apps/ensadmin/src/app/@actions/api/rest/page.tsx
+++ apps/ensadmin/src/app/@actions/api/rest/page.tsx
@@ -9,8 +9,10 @@ export default function Actions() {
   const url = useOpenApiUrl();
 
   return (
-    <div className="flex w-full max-w-md items-center space-x-2">
-      <span className="font-mono text-xs select-none text-gray-500">{url}</span>
+    <div className="flex w-full max-w-md min-w-0 items-center space-x-2">
+      <span className="font-mono text-xs select-none text-gray-500 truncate min-w-0" title={url}>
+        {url}
+      </span>
       <CopyButton
         value={url}
         message="URL copied to clipboard!"
