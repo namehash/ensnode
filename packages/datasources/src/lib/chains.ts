e6c312b033417752fc7239b11f117c5027199601
sevenzing <lymarenko.lev@gmail.com>
2026-05-04 17:25:38 +0400

    fix comment

--- packages/datasources/src/lib/chains.ts
+++ packages/datasources/src/lib/chains.ts
@@ -1,14 +1,11 @@
 import { anvil, type Chain, sepolia } from "viem/chains";
 
 /**
- * The ens-test-env chain id is 1:
- * @see https://github.com/ensdomains/contracts-v2/blob/9f26a8f01f1f87db1c5d57b9faa8e76f0c5043ef/contracts/script/setup.ts#L91
+ * NOTE: devnet uses anvil's default chain id of 31337, but we over-specify it here for documentation
+ * @see https://github.com/ensdomains/contracts-v2/blob/580c60a20e80decce21cf15aafd762f96a96d544/contracts/script/setup.ts#L55
  */
-
 export const ensTestEnvChain = {
   ...anvil,
-  // NOTE: devnet uses anvil's default chain id of 31337, but we over-specify it here for documentation
-  // https://github.com/ensdomains/contracts-v2/blob/580c60a20e80decce21cf15aafd762f96a96d544/contracts/script/setup.ts#L55
   id: 31337,
   name: "ens-test-env",
 } as const satisfies Chain;
