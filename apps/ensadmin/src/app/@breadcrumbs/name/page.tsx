456c3e5db5ec8060810d834cead10c74d0b1881c
Jamie Barton <jamie@notrab.dev>
2026-05-07 15:41:53 +0100

    lint

--- apps/ensadmin/src/app/@breadcrumbs/name/page.tsx
+++ apps/ensadmin/src/app/@breadcrumbs/name/page.tsx
@@ -18,6 +18,7 @@ export default function Page() {
   const searchParams = useSearchParams();
   const { retainCurrentRawConnectionUrlParam } = useRawConnectionUrlParam();
   const exploreNamesBaseHref = retainCurrentRawConnectionUrlParam("/name");
+
   const name = (searchParams.get("name")?.trim() || null) as Name | null;
 
   return (
