f3d7abc28a5f765ad49055d35daf276c4453b1ea
Jamie Barton <jamie@notrab.dev>
2026-05-11 09:22:34 +0100

    use internal pkg to identify another enskit method

--- examples/ensnode-react-example/src/config.ts
+++ examples/ensnode-react-example/src/config.ts
@@ -1,34 +1,13 @@
 import { type ENSNamespaceId, ENSNamespaceIds } from "@ensnode/datasources";
+import { ENSNamespaceSchema, makeUrlSchema } from "@ensnode/ensnode-sdk/internal";
 
+// TODO: potential internal to be made public
 const DEFAULT_ENSNODE_URL = "https://api.alpha.ensnode.io";
 
-const VALID_NAMESPACE_IDS: readonly ENSNamespaceId[] = Object.values(ENSNamespaceIds);
-
-function isENSNamespaceId(value: string): value is ENSNamespaceId {
-  return VALID_NAMESPACE_IDS.some((id) => id === value);
-}
-
-function parseEnsNodeUrl(value: string): URL {
-  try {
-    return new URL(value);
-  } catch {
-    throw new Error(`VITE_ENSNODE_URL must be a valid URL. Got: '${value}'.`);
-  }
-}
-
-function parseExpectedNamespace(value: string): ENSNamespaceId {
-  if (!isENSNamespaceId(value)) {
-    throw new Error(
-      `VITE_ENS_NAMESPACE must be one of: ${VALID_NAMESPACE_IDS.join(", ")}. Got: '${value}'.`,
-    );
-  }
-  return value;
-}
-
-export const ENSNODE_URL: URL = parseEnsNodeUrl(
+export const ENSNODE_URL: URL = makeUrlSchema("VITE_ENSNODE_URL").parse(
   import.meta.env.VITE_ENSNODE_URL ?? DEFAULT_ENSNODE_URL,
 );
 
-export const EXPECTED_NAMESPACE: ENSNamespaceId = parseExpectedNamespace(
+export const EXPECTED_NAMESPACE: ENSNamespaceId = ENSNamespaceSchema.parse(
   import.meta.env.VITE_ENS_NAMESPACE ?? ENSNamespaceIds.Mainnet,
 );
