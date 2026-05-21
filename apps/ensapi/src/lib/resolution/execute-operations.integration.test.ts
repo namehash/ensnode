6a72955a3ba5ba391151caf10c3b5dca3cd69ed2
Tomasz Kopacki <tomasz@kopacki.net>
2026-05-21 11:12:54 +0200

    Fix integration testing suite

--- apps/ensapi/src/lib/resolution/execute-operations.integration.test.ts
+++ apps/ensapi/src/lib/resolution/execute-operations.integration.test.ts
@@ -1,14 +1,20 @@
+import { createPublicClient, http } from "viem";
 import { vi } from "vitest";
 
-import { ENSNamespaceIds, ensTestEnvChain } from "@ensnode/datasources";
+import { ENSNamespaceIds } from "@ensnode/datasources";
 
 // we're testing a function specifically, not fetching through the running ensapi instance, so
 // we need to mock the config when this worker process attempts to import ./execute-operations
 // (and this is an integration test because we want to RPC fetch against the running devnet)
-vi.mock("@/config", () => ({
+vi.mock("@/di", () => ({
   default: {
-    namespace: ENSNamespaceIds.EnsTestEnv,
-    rpcConfigs: new Map([[ensTestEnvChain.id, { httpRPCs: [new URL("http://localhost:8545")] }]]),
+    context: {
+      get rootChainPublicClient() {
+        return createPublicClient({
+          transport: http("http://localhost:8545"),
+        });
+      },
+    },
   },
 }));
 
