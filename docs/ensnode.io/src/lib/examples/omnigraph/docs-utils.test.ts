45d8fd41131fe60e55eb07258b13b76659a72d1b
sevenzing <lymarenko.lev@gmail.com>
2026-06-15 16:03:50 +0300

    lol, thanks greptile!

--- docs/ensnode.io/src/lib/examples/omnigraph/docs-utils.test.ts
+++ docs/ensnode.io/src/lib/examples/omnigraph/docs-utils.test.ts
@@ -1,17 +1,8 @@
 import { describe, expect, it } from "vitest";
-
-import exampleSnapshots from "@data/omnigraph-examples/examples.json";
-import type { SnapshotExample } from "@data/omnigraph-examples/types";
-
 import { buildOmnigraphCurlExample } from "./docs-utils";
-
-const examples = exampleSnapshots as SnapshotExample[];
 
 describe("buildOmnigraphCurlExample", () => {
   it("strips # line comments before compacting the query", () => {
-    const offchainName = examples.find((example) => example.id === "offchain-name");
-    expect(offchainName).toBeDefined();
-
     const curl = buildOmnigraphCurlExample({
       connectionBaseUrl: "https://api.alpha.ensnode.io",
       query: `query SimpleQuery($name: InterpretedName!) {
